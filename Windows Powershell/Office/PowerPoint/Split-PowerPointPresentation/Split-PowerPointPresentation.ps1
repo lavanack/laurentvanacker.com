3203e38a5a37012dac18122ad4567ba925a5325d
lavanack <lavanack@microsoft.com>
2020-07-30 11:13:55 +0200

    Adding a slit mode (section or slide)

--- Windows Powershell/Office/PowerPoint/Split-PowerPointPresentation/Split-PowerPointPresentation.ps1
+++ Windows Powershell/Office/PowerPoint/Split-PowerPointPresentation/Split-PowerPointPresentation.ps1
@@ -20,23 +20,24 @@ Function Remove-Ref {
 Function Split-PowerPointPresentation {
 	<#
 			.SYNOPSIS
-			Split PowerPoint presentations with section with on dedicated file per section
+			Split PowerPoint presentations with section with on dedicated file per section or slide
 
 			.DESCRIPTION
-			Split PowerPoint presentations with section with on dedicated file per section
+			Split PowerPoint presentations with section with on dedicated file per section or slide
 
 			.PARAMETER Source
 			The PowerPoint presentation files to split specified by its full name
 
 			.EXAMPLE
 			$Get-ChildItem -Path $CurrentDir -filter *.pptx | Sort-Object -Property Name | Split-PowerPointPresentation -Verbose -Open
-			Will Split all the PowerPoint files into the current directory into multiple single Powerpoint files (one file per section) 
+			Will Split all the PowerPoint files into the current directory into multiple single Powerpoint files (one file per section - Default mode) 
 			The output will be verbose
 			The PowerPoint application won't be left after the processing
 
 			.EXAMPLE
 			$Presentations = "$CurrentDir\0.pptx", "$CurrentDir\PresentationToSplit1.pptx", "$CurrentDir\PresentationToSplit2.pptx"
-			Will Split all the PowerPoint files into the current directory into multiple single Powerpoint files (one file per section) 
+            Split-PowerPointPresentation -Source $Presentations -Verbose -Mode Slide
+			Will Split all the PowerPoint files into the current directory into multiple single Powerpoint files (one file per slide) 
 			Will Split all the specified PowerPoint files into into the C:\Temp\SplitdPresentation.pptx Powerpoint file
 	#>
 	[CmdletBinding()]
@@ -47,7 +48,9 @@ Function Split-PowerPointPresentation {
 				(Test-Path -Path $_ -PathType Leaf) -and ($_ -match "\.ppt(x{0,1})$")
 			})]
 		[alias('FilePath', 'Path', 'FullName')]
-		[string[]]$Source
+		[string[]]$Source,
+        [ValidateSet('Section', 'Slide')]
+		[string[]]$Mode='Section'
 	)
 	begin {
 		#Opening the PowerPoint application once
@@ -60,25 +63,48 @@ Function Split-PowerPointPresentation {
 			#Getting the base name of the processed presentation
 			Write-Verbose -Message "Opening (Read-only mode) $CurrentSourcePath ..."
             $CurrentSrcPresentation = $Powerpoint.Presentations.Open($CurrentSourcePath, [Microsoft.Office.Core.MsoTriState]::msoTrue)
-            $SlideStart = 1
-    		for ($SrcSectionIndex=1; $SrcSectionIndex -le $CurrentSrcPresentation.SectionProperties.count; $SrcSectionIndex++) {
-                if ($CurrentSourcePath -match "(?<filename>.*)\.(?<extension>\w+)")
-                {
-                    $CurrentDestPath = $Matches["filename"]+$("_{0:D3}_{1}." -f $SrcSectionIndex, $CurrentSrcPresentation.SectionProperties.Name($SrcSectionIndex))+$Matches["extension"]
-                    Write-Verbose "Processing $($CurrentDestPath) ..."
-                    Copy-Item -Path $CurrentSourcePath -Destination $CurrentDestPath -Force
-        			Write-Verbose -Message "Opening (Write mode) $CurrentDestPath  ..."
-                    $CurrentDestPresentation = $Powerpoint.Presentations.Open($CurrentDestPath, [Microsoft.Office.Core.MsoTriState]::msoFalse)
-                    #Moving the section to keep in the first position
-                    $CurrentDestPresentation.SectionProperties.Move($SrcSectionIndex, 1)
-                    #Removing all other sections
-            		2 .. $CurrentDestPresentation.SectionProperties.count | ForEach-Object {
-                		Write-Verbose -Message "Deleting section : $($CurrentDestPresentation.SectionProperties.Name(2))  ..."
-                        $CurrentDestPresentation.SectionProperties.Delete(2, $True)
+            if ($Mode -eq 'Section')
+            {
+    		    for ($SrcSectionIndex=1; $SrcSectionIndex -le $CurrentSrcPresentation.SectionProperties.count; $SrcSectionIndex++) {
+                    if ($CurrentSourcePath -match "(?<filename>.*)\.(?<extension>\w+)")
+                    {
+                        $CurrentDestPath = $Matches["filename"]+$("_{0:D3}_{1}." -f $SrcSectionIndex, $CurrentSrcPresentation.SectionProperties.Name($SrcSectionIndex))+$Matches["extension"]
+                        Write-Verbose "Processing $($CurrentDestPath) ..."
+                        Copy-Item -Path $CurrentSourcePath -Destination $CurrentDestPath -Force
+        			    Write-Verbose -Message "Opening (Write mode) $CurrentDestPath  ..."
+                        $CurrentDestPresentation = $Powerpoint.Presentations.Open($CurrentDestPath, [Microsoft.Office.Core.MsoTriState]::msoFalse)
+                        #Moving the section to keep in the first position
+                        $CurrentDestPresentation.SectionProperties.Move($SrcSectionIndex, 1)
+                        #Removing all other sections
+            		    2 .. $CurrentDestPresentation.SectionProperties.count | ForEach-Object {
+                		    Write-Verbose -Message "Deleting section : $($CurrentDestPresentation.SectionProperties.Name(2))  ..."
+                            $CurrentDestPresentation.SectionProperties.Delete(2, $True)
+                        }
+                        $CurrentDestPresentation.Save()
+                        $CurrentDestPresentation.Close()
+			            Remove-Ref -ref ($CurrentDestPresentation)
                     }
-                    $CurrentDestPresentation.Save()
-                    $CurrentDestPresentation.Close()
-			        Remove-Ref -ref ($CurrentDestPresentation)
+                }
+            }
+            elseif ($Mode -eq 'Slide')
+            {
+    		    for ($SrcSlideIndex=1; $SrcSlideIndex -le $CurrentSrcPresentation.Slides.Count; $SrcSlideIndex++) {
+                    if ($CurrentSourcePath -match "(?<filename>.*)\.(?<extension>\w+)")
+                    {
+                        $CurrentDestPath = $Matches["filename"]+$("_{0:D3}." -f $SrcSlideIndex)+$Matches["extension"]
+                        Write-Verbose "Creating $($CurrentDestPath) ..."
+                        $CurrentDestPresentation = $Powerpoint.Presentations.Add($True)
+
+			            #Inserting one slide of the current presentation to the new one
+			            $InsertedSlidesNb = $CurrentDestPresentation.Slides.InsertFromfile($CurrentSourcePath, 0, $SrcSlideIndex, $SrcSlideIndex)
+			
+			            #Applying the original template
+			            $CurrentDestPresentation.Slides.Range(1..1).ApplyTemplate($CurrentSourcePath)
+
+                        $CurrentDestPresentation.SaveAs($CurrentDestPath)
+                        $CurrentDestPresentation.Close()
+			            Remove-Ref -ref ($CurrentDestPresentation)
+                    }
                 }
             }
             $CurrentSrcPresentation.Close()
@@ -98,7 +124,7 @@ Clear-Host
 $CurrentDir = Split-Path -Path $MyInvocation.MyCommand.Path
 
 #Example 1 : Processing all the PowerPoint presentation in current directory in the alphabetical order
-Get-ChildItem -Path $CurrentDir -Filter "PresentationToSplit?.pptx" -File | Split-PowerPointPresentation -Verbose
+Get-ChildItem -Path $CurrentDir -Filter "PresentationToSplit?.pptx" -File | Split-PowerPointPresentation -Verbose -Mode Slide
 
 #Example 2 : Processing a list of some PowerPoint presentations specified by their absolute path
 #$Presentations = "$CurrentDir\0.pptx", "$CurrentDir\PresentationToSplit1.pptx", "$CurrentDir\PresentationToSplit2.pptx"
