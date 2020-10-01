91650e52178e6e8a9a41825102eec60ce5b011bc
lavanack <lavanack@microsoft.com>
2020-10-01 16:58:56 +0200

    Initial commit

---
+++ Powershell/Remoting over SSH/AutomatedLab - Windows 10.ps1
@@ -0,0 +1,116 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5 -Modules AutomatedLab -RunAsAdministrator 
+trap {
+    Write-Host "Stopping Transcript ..."; Stop-Transcript
+} 
+Clear-Host
+$PreviousVerbosePreference = $VerbosePreference
+$VerbosePreference = 'SilentlyContinue'
+$PreviousErrorActionPreference = $ErrorActionPreference
+$ErrorActionPreference = 'Stop'
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+$TranscriptFile = $CurrentScript -replace ".ps1$", "_$("{0:yyyyMMddHHmmss}" -f (Get-Date)).txt"
+Start-Transcript -Path $TranscriptFile -IncludeInvocationHeader
+
+#region Global variables definition
+$Logon = 'Administrator'
+$ClearTextPassword = 'P@ssw0rd'
+$SecurePassword = ConvertTo-SecureString -String $ClearTextPassword -AsPlainText -Force
+$NetBiosDomainName = 'CONTOSO'
+$FQDNDomainName = 'contoso.com'
+
+$NetworkID = '10.0.0.0/16' 
+$DC01IPv4Address = '10.0.0.1'
+$WINDOWSIPv4Address = '10.0.0.10'
+
+#Using half of the logical processors to speed up the deployement
+[int]$LabMachineDefinitionProcessors = [math]::Max(1, (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors)
+
+$LabName = 'CrossPlatformPSRemoting'
+#endregion
+
+#Cleaning previously existing lab
+if ($LabName -in (Get-Lab -List)) {
+    Remove-Lab -Name $LabName -Confirm:$false -ErrorAction SilentlyContinue
+}
+
+#create an empty lab template and define where the lab XML files and the VMs will be stored
+New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV
+
+#make the network definition
+Add-LabVirtualNetworkDefinition -Name $LabName -HyperVProperties @{
+    SwitchType = 'Internal'
+} -AddressSpace $NetworkID
+Add-LabVirtualNetworkDefinition -Name 'Default Switch' -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Wi-Fi' }
+
+#and the domain definition with the domain admin account
+Add-LabDomainDefinition -Name $FQDNDomainName -AdminUser $Logon -AdminPassword $ClearTextPassword
+
+#these credentials are used for connecting to the machines. As this is a lab we use clear-text passwords
+Set-LabInstallationCredential -Username $Logon -Password $ClearTextPassword
+
+#defining default parameter values, as these ones are the same for all the machines
+$PSDefaultParameterValues = @{
+    'Add-LabMachineDefinition:Network'         = $LabName
+    'Add-LabMachineDefinition:DomainName'      = $FQDNDomainName
+    'Add-LabMachineDefinition:MinMemory'       = 1GB
+    'Add-LabMachineDefinition:MaxMemory'       = 2GB
+    'Add-LabMachineDefinition:Memory'          = 2GB
+    'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2019 Standard (Desktop Experience)'
+    'Add-LabMachineDefinition:Processors'      = $LabMachineDefinitionProcessors
+}
+
+$WINDOWSNetAdapter = @()
+$WINDOWSNetAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $LabName -Ipv4Address $WINDOWSIPv4Address
+$WINDOWSNetAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch 'Default Switch' -UseDhcp
+
+#region server definitionsS
+#Domain controller
+Add-LabMachineDefinition -Name DC01 -Roles RootDC -IpAddress $DC01IPv4Address
+#Windows 10
+Add-LabMachineDefinition -Name WINDOWS -NetworkAdapter $WINDOWSNetAdapter -OperatingSystem 'Windows 10 Enterprise'
+#endregion
+
+#Installing servers
+Install-Lab
+
+
+#region Installing Required Windows Features
+$machines = Get-LabVM
+#endregion
+
+Invoke-LabCommand -ActivityName "Disabling IE ESC" -ComputerName $machines -ScriptBlock {
+    #Setting the Keyboard to French
+    Set-WinUserLanguageList -LanguageList "fr-FR" -Force
+}
+
+#Setting processor number to 1 for all VMs
+Get-LabVM -All | Stop-VM -Passthru | Set-VMProcessor -Count 1
+Get-LabVM -All | Start-VM
+
+Show-LabDeploymentSummary -Detailed
+Checkpoint-LabVM -SnapshotName 'FullInstall' -All
+
+$VerbosePreference = $PreviousVerbosePreference
+$ErrorActionPreference = $PreviousErrorActionPreference
+#Restore-LabVMSnapshot -SnapshotName 'FullInstall' -All -Verbose
+
+Stop-Transcript
