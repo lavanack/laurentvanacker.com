8f1c878c82643e2fc1deae543cf45071e1786eeb
lavanack <lavanack@microsoft.com>
2020-10-16 12:28:36 +0200

    Minor changes

--- Windows Powershell/Exchange Online/Get-MeetingRoomUtilizationRate.ps1
+++ Windows Powershell/Exchange Online/Get-MeetingRoomUtilizationRate.ps1
@@ -1,4 +1,21 @@
-﻿
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#For an Microsoft 365 Test Tenant : http://demos.microsoft.com/
 #requires -version 3
 
 #=======================================================================================
@@ -52,7 +69,7 @@ Function Get-AccessToken
     $null = [System.Reflection.Assembly]::LoadFrom($adalforms)
     $authority = "https://login.microsoftonline.com/$TenantName"
     $authContext = New-Object -TypeName 'Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext' -ArgumentList $authority
-    $platformParameters = New-Object -TypeName 'Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters'    -ArgumentList $CredPrompt
+    $platformParameters = New-Object -TypeName 'Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters' -ArgumentList $CredPrompt
     $authenticationResult = $authContext.AcquireTokenAsync($resourceAppIdURI, $ClientID, $redirectUri, $platformParameters)
     
     if ( !$authenticationResult.IsFaulted )
@@ -1002,17 +1019,17 @@ catch
     throw 'Prerequisites not installed (AzureAD PowerShell module not installed)'
 }
 
-#Azure AD only applications v1.0 : Web app / API ==> EWSWebApp
+#Azure AD only applications v1.0 : Web app / API ==> EWSNativeApp
 $resourceAppIdURI = 'https://outlook.office365.com'
-$ClientID = '123456789-1234-1234-1234-123456789012'   #AKA Application ID
-$TenantName = 'yourtenant.onmicrosoft.com'            #Your Tenant Name
-$CredPrompt = 'Auto'                                 #Auto, Always, Never, RefreshSession
+$ClientID = '12345678-1234-1234-1234-123456789012'   #AKA Application ID
+$TenantName = 'tenant.onmicrosoft.com'            #Your Tenant Name
+$CredPrompt = 'Always'                                 #Auto, Always, Never, RefreshSession
 $redirectUri = 'http://localhost'                    #Your Application's Redirect URI
 $Method = 'Get'                                      #GET or PATCH
 
 $MeetingRoomData = $null
 #Getting meeting room data
-$Whoami = 'you@contoso.com'
+$Whoami = 'user@tenant.OnMicrosoft.com'
 $Start = Get-Date
 
 $isImportSuccessful = Import-EWSManagedAPI -Verbose
@@ -1023,7 +1040,7 @@ if (!$isImportSuccessful)
 
 $AccessToken = Get-AccessToken -TenantName $TenantName -ClientID $ClientID -redirectUri $redirectUri -resourceAppIdURI $resourceAppIdURI -CredPrompt $CredPrompt -Verbose 
 $ExchangeService = New-EWSExchangeService -MailBox $Whoami -URL 'https://outlook.office365.com/EWS/Exchange.asmx' -AccessToken $AccessToken -Office365 -AllowInsecureRedirection -Verbose #-Trace 
-$EWSRoomLists = Get-EWSRoomList -ExchangeService $ExchangeService -Address -Filter "*france*"
+$EWSRoomLists = Get-EWSRoomList -ExchangeService $ExchangeService -Address #-Filter "*france*"
 $MeetingRoomData = $EWSRoomLists | Get-EWSMeetingRoomData -ExchangeService $ExchangeService -Skip Saturday, Sunday -Verbose #-Filter "*Paris*"
 
 #Measuring meeting room utilization
