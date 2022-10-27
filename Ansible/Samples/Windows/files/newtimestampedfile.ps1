1534b96d469994dcc0aef8709e6ae65b51a59223
Laurent VAN ACKER <laurent.vanacker@free.fr>
2022-10-27 20:33:30 +0200

    Fix

--- Ansible/Samples/Windows/files/newtimestampedfile.ps1
+++ Ansible/Samples/Windows/files/newtimestampedfile.ps1
@@ -1,3 +1,3 @@
 $Now = Get-Date
 $TimeStamp = "{0:yyyyMMddHHmmss}.txt" -f $Now
-New-Item -Path $env:SystemRoot -Name $TimeStamp -ItemType File -Force -Value "This file has been generated via Ansible Tower at $Now"
+New-Item -Path $env:SystemDrive\ -Name $TimeStamp -ItemType File -Force -Value "This file has been generated via Ansible Tower at $Now"
