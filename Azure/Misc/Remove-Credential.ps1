01a593fab6ec9f3ff883013ddb0b2427b4d2e50e
Laurent VAN ACKER <laurent.vanacker@free.fr>
2023-03-03 17:32:08 +0100

    Initial Commit

--- Azure/Misc/Remove-Credential.ps1
+++ Azure/Misc/Remove-Credential.ps1
@@ -1,17 +1,20 @@
 ﻿Clear-Host
 $AzureCredentials = cmdkey /list | Select-string -Pattern "=(TERMSRV/)?((.*)\.(.*)\.cloudapp\.azure\.com)" -AllMatches
-$AzureCredentials.Matches | ForEach-Object -Process { 
-    $DNSName = $_.Groups[2].Value
-    $VMName = $_.Groups[3].Value
-    $Location = $_.Groups[4].Value
-    $AzVM = Get-AzVM -Name $VMName 
-    if (($AzVM) -and ($AzVM.Location -eq $Location))
-    {
-        Write-Host -Object "$VMName Azure VM exists" -ForegroundColor Green
-    }
-    else
-    {
-        Write-Warning -Message "$VMName Azure VM doesn't exist"
-        Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "cmdkey /delete:$DNSName" -Wait
+if ($AzureCredentials.Matches)
+{
+    $AzureCredentials.Matches | ForEach-Object -Process { 
+        $DNSName = $_.Groups[2].Value
+        $VMName = $_.Groups[3].Value
+        $Location = $_.Groups[4].Value
+        $AzVM = Get-AzVM -Name $VMName 
+        if (($AzVM) -and ($AzVM.Location -eq $Location))
+        {
+            Write-Host -Object "$VMName Azure VM exists" -ForegroundColor Green
+        }
+        else
+        {
+            Write-Warning -Message "$VMName Azure VM doesn't exist"
+            Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "cmdkey /delete:$DNSName" -Wait
+        }
     }
 }
