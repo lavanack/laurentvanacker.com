95b7599447f0fa14c69b962eaefd1b00360c16d8
Laurent VAN ACKER <laurent.vanacker@free.fr>
2023-06-04 19:29:42 +0200

    Minor improvements

--- Azure/Azure Virtual Machines/Get-AzRunningVMJITAccess.ps1
+++ Azure/Azure Virtual Machines/Get-AzRunningVMJITAccess.ps1
@@ -8,20 +8,29 @@ $MyPublicIp           = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 $RunningVM            = Get-AzVM -Status | Where-Object -FilterScript {$_.PowerState -match "running"}
 #endregion
 
+$JitNetworkAccessPolicyVM = ((Get-AzJitNetworkAccessPolicy | Where-Object -FilterScript {$_.Name -eq $JitPolicyName})).VirtualMachines.Id
+
 #region Requesting Temporary Access : 3 hours
 $AzJitNetworkAccessPolicy = foreach ($VM in $RunningVM)
 {
-    $JitPolicy = (@{
-            id    = $VM.Id
-            ports = (@{
-                    number                     = $RDPPort;
-                    endTimeUtc                 = (Get-Date).AddHours(3).ToUniversalTime()
-                    allowedSourceAddressPrefix = @($MyPublicIP) 
-                })
-        })
     $ActivationVM = @($JitPolicy)
-    Write-Host "Requesting Temporary Acces via Just in Time for $($VM.Name) on port number $RDPPort for maximum $JitPolicyTimeInHours hours from $MyPublicIp ..."
-    Start-AzJitNetworkAccessPolicy -ResourceGroupName $($VM.ResourceGroupName) -Location $VM.Location -Name $JitPolicyName -VirtualMachine $ActivationVM
+    if ($VM.Id -in $JitNetworkAccessPolicyVM)
+    {
+        $JitPolicy = (@{
+                id    = $VM.Id
+                ports = (@{
+                        number                     = $RDPPort;
+                        endTimeUtc                 = (Get-Date).AddHours(3).ToUniversalTime()
+                        allowedSourceAddressPrefix = @($MyPublicIP) 
+                    })
+                })
+        Write-Host -Object "Requesting Temporary Acces via Just in Time for $($VM.Name) on port number $RDPPort for maximum $JitPolicyTimeInHours hours from $MyPublicIp ..."
+        Start-AzJitNetworkAccessPolicy -ResourceGroupName $($VM.ResourceGroupName) -Location $VM.Location -Name $JitPolicyName -VirtualMachine $ActivationVM | Select-Object -Property *, @{Name='endTimeUtc'; Expression={$JitPolicy.ports.endTimeUtc}}
+    }
+    else
+    {
+        Write-Warning -Message "Just in Time for is not enabled for $($VM.Name)"
+    }
 }
 #Diplaying the overall result
 $AzJitNetworkAccessPolicy | Format-List * -Force
