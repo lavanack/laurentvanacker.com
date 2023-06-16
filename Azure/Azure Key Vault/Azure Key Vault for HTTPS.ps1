3fd59588d61fd91ba70423c8f9fc06c0efb49f8f
Laurent VAN ACKER <laurent.vanacker@free.fr>
2023-06-16 12:57:38 +0200

    Updates

--- Azure/Azure Key Vault/Azure Key Vault for HTTPS.ps1
+++ Azure/Azure Key Vault/Azure Key Vault for HTTPS.ps1
@@ -1,20 +1,316 @@
-﻿Clear-Host
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5 -Modules Az.Compute, Az.Network, Az.Storage, Az.Resources
+
+[CmdletBinding()]
+param
+(
+)
+
+
+#region function definitions 
+#Based from https://adamtheautomator.com/powershell-random-password/
+function New-RandomPassword {
+    [CmdletBinding(PositionalBinding = $false)]
+    param
+    (
+        [int] $minLength = 12, ## characters
+        [int] $maxLength = 15, ## characters
+        [int] $nonAlphaChars = 3,
+        [switch] $AsSecureString,
+        [switch] $ClipBoard
+    )
+
+    Add-Type -AssemblyName 'System.Web'
+    $length = Get-Random -Minimum $minLength -Maximum $maxLength
+    $RandomPassword = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
+    Write-Host "The password is : $RandomPassword"
+    if ($ClipBoard) {
+        Write-Verbose "The password has beeen copied into the clipboard (Use Win+V) ..."
+        $RandomPassword | Set-Clipboard
+    }
+    if ($AsSecureString) {
+        ConvertTo-SecureString -String $RandomPassword -AsPlainText -Force
+    }
+    else {
+        $RandomPassword
+    }
+}
+#endregion
+
+Clear-Host
+$Error.Clear()
+
 $CurrentScript = $MyInvocation.MyCommand.Path
 #Getting the current directory (where this script file resides)
 $CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir 
 
-$ResourcePrefix     = "dscvmext"
-$VM = Get-AzVM -Name $ResourcePrefix* 
-$Location           = $VM.Location
-$ResourceGroupName  = $VM.ResourceGroupName
-$VMName 	        = $VM.Name
-$FQDN               = "$VMName.$Location.cloudapp.azure.com".ToLower()
-$KeyVaultName       = "{0}keyvault" -f $VMName
-$CertificateName    = "mycert"
-#$UserPrincipalName  = (Get-AzContext).Account.Id
-#From https://learn.microsoft.com/en-us/azure/virtual-machines/windows/tutorial-secure-web-server
+#region Defining variables 
+$SubscriptionName = "Cloud Solution Architect"
+#region Building an Hashtable to get the shortname of every Azure location based on a JSON file on the Github repository of the Azure Naming Tool
+$AzLocation = Get-AzLocation | Select-Object -Property Location, DisplayName | Group-Object -Property DisplayName -AsHashTable -AsString
+$ANTResourceLocation = Invoke-RestMethod -Uri https://raw.githubusercontent.com/microsoft/CloudAdoptionFramework/master/ready/AzNamingTool/repository/resourcelocations.json
+$shortNameHT = $ANTResourceLocation | Select-Object -Property name, shortName, @{Name = 'Location'; Expression = { $AzLocation[$_.name].Location } } | Where-Object -FilterScript { $_.Location } | Group-Object -Property Location -AsHashTable -AsString
+#endregion
 
-#region Adding the DSC extension
+# Login to your Azure subscription.
+While (-not((Get-AzContext).Subscription.Name -eq $SubscriptionName)) {
+    Connect-AzAccount
+    Get-AzSubscription | Out-GridView -OutputMode Single -Title "Select your Azure Subscription" | Select-AzSubscription
+    #$Subscription = Get-AzSubscription -SubscriptionName $SubscriptionName -ErrorAction Ignore
+    #Select-AzSubscription -SubscriptionName $SubscriptionName | Select-Object -Property *
+}
+
+$AzureVMNameMaxLength = 15
+$RDPPort = 3389
+$JitPolicyTimeInHours = 3
+$JitPolicyName = "Default"
+$Location = "eastus"
+$VMSize = "Standard_D4s_v5"
+$LocationShortName = $shortNameHT[$Location].shortName
+#Naming convention based on https://github.com/microsoft/CloudAdoptionFramework/tree/master/ready/AzNamingTool
+$KeyVaultPrefix = "kv"
+$ResourceGroupPrefix = "rg"
+$StorageAccountPrefix = "sa"
+$VirtualMachinePrefix = "vm"
+$NetworkSecurityGroupPrefix = "nsg"
+$VirtualNetworkPrefix = "vnet"
+$SubnetPrefix = "vnets"
+$Project = "kv"
+$Role = "https"
+$DigitNumber = 4
+$Instance = Get-Random -Minimum 0 -Maximum $([long]([Math]::Pow(10, $DigitNumber)))
+$CertificateName = "mycert"
+
+Do {
+    $StorageAccountName = "{0}{1}{2}{3}{4:D$DigitNumber}" -f $StorageAccountPrefix, $Project, $Role, $LocationShortName, $Instance                       
+    $VMName = "{0}{1}{2}{3}{4:D$DigitNumber}" -f $VirtualMachinePrefix, $Project, $Role, $LocationShortName, $Instance                       
+} While ((-not(Test-AzDnsAvailability -DomainNameLabel $VMName -Location $Location)) -or ((-not(Get-AzStorageAccountNameAvailability -Name $StorageAccountName).NameAvailable)))
+                         
+$NetworkSecurityGroupName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $NetworkSecurityGroupPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$VirtualNetworkName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $VirtualNetworkPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$SubnetName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $SubnetPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$ResourceGroupName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $ResourceGroupPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$KeyVaultName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $KeyVaultPrefix, $Project, $Role, $LocationShortName, $Instance                       
+
+$StorageAccountName = $StorageAccountName.ToLower()
+$VMName = $VMName.ToLower()
+$NetworkSecurityGroupName = $NetworkSecurityGroupName.ToLower()
+$VirtualNetworkName = $VirtualNetworkName.ToLower()
+$SubnetName = $SubnetName.ToLower()
+$ResourceGroupName = $ResourceGroupName.ToLower()
+$VirtualNetworkAddressSpace = "10.10.0.0/16" # Format 10.10.0.0/16
+$SubnetIPRange = "10.10.1.0/24" # Format 10.10.1.0/24                         
+$FQDN = "$VMName.$Location.cloudapp.azure.com".ToLower()
+
+
+#region Defining credential(s)
+$Username = $env:USERNAME
+#$ClearTextPassword = 'I@m@JediLikeMyF@therB4Me'
+#$ClearTextPassword = New-RandomPassword -ClipBoard -Verbose
+#$SecurePassword = ConvertTo-SecureString -String $ClearTextPassword -AsPlainText -Force
+$SecurePassword = New-RandomPassword -ClipBoard -AsSecureString -Verbose
+$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList ($Username, $SecurePassword)
+#endregion
+
+$ResourceGroup = Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction Ignore 
+if ($ResourceGroup) {
+    #Step 0: Remove previously existing Azure Resource Group with the same name
+    $ResourceGroup | Remove-AzResourceGroup -Force -Verbose
+}
+$MyPublicIp = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
+$DSCFileName = "WebServerDSC.ps1"
+$DSCFilePath = Join-Path -Path $CurrentDir -ChildPath $DSCFileName
+$ConfigurationName = "WebServerConfiguration"
+
+
+#region Define Variables needed for Virtual Machine
+$ImagePublisherName = "MicrosoftWindowsServer"
+$ImageOffer = "WindowsServer"
+$ImageSku = "2022-datacenter-g2"
+$PublicIPName = "pip-$VMName" 
+$NICName = "nic-$VMName"
+$OSDiskName = '{0}_OSDisk' -f $VMName
+#$DataDiskName = "$VMName-DataDisk01"
+$OSDiskSize = "127"
+$StorageAccountSkuName = "Standard_LRS"
+$OSDiskType = "Standard_LRS"
+
+Write-Verbose "`$VMName: $VMName"
+Write-Verbose "`$NetworkSecurityGroupName: $NetworkSecurityGroupName"         
+Write-Verbose "`$VirtualNetworkName: $VirtualNetworkName"         
+Write-Verbose "`$SubnetName: $SubnetName"       
+Write-Verbose "`$ResourceGroupName: $ResourceGroupName"
+Write-Verbose "`$PublicIPName: $PublicIPName"
+Write-Verbose "`$NICName: $NICName"
+Write-Verbose "`$OSDiskName: $OSDiskName"
+Write-Verbose "`$FQDN: $FQDN"
+#endregion
+#endregion
+
+
+if ($VMName.Length -gt $AzureVMNameMaxLength) {
+    Write-Error "'$VMName' exceeds $AzureVMNameMaxLength characters" -ErrorAction Stop
+}
+elseif (-not($LocationShortName)) {
+    Write-Error "No location short name found for '$Location'" -ErrorAction Stop
+}
+elseif ($null -eq (Get-AZVMSize -Location $Location | Where-Object -FilterScript { $_.Name -eq $VMSize })) {
+    Write-Error "The '$VMSize' is not available in the '$Location' location ..." -ErrorAction Stop
+}
+
+#Step 1: Create Azure Resource Group
+# Create Resource Groups and Storage Account for diagnostic
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force
+
+#Step 2: Create Azure Storage Account
+New-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroupName -Location $Location -SkuName $StorageAccountSkuName
+
+#Step 3: Create Azure Network Security Group
+#RDP only for my public IP address
+$CommonParameters = @{
+    'SourceAddressPrefix' = 'VirtualNetwork'
+    'SourcePortRange' = '*'
+    'DestinationAddressPrefix' = $ADSubnetAddressRange
+    'Access' = 'Allow'
+    'Direction' = 'Inbound' 
+}
+$SecurityRules = @(
+    #region Inbound
+    #RDP only for my public IP address
+    New-AzNetworkSecurityRuleConfig -Name RDPRule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 300 -SourceAddressPrefix $MyPublicIp -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange $RDPPort
+    #HTTP only for my public IP address
+    New-AzNetworkSecurityRuleConfig -Name HTTPRule -Description "Allow HTTP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 301 -SourceAddressPrefix $MyPublicIp -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+    #HTTPS only for my public IP address
+    New-AzNetworkSecurityRuleConfig -Name HTTPSRule -Description "Allow HTTPS" -Access Allow -Protocol Tcp -Direction Inbound -Priority 302 -SourceAddressPrefix $MyPublicIp -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 443
+    #endregion
+)
+
+$NetworkSecurityGroup = New-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Location $Location -Name $NetworkSecurityGroupName -SecurityRules $SecurityRules -Force
+
+#Steps 4 + 5: Create Azure Virtual network using the virtual network subnet configuration
+$VirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $VirtualNetworkName  -AddressPrefix $VirtualNetworkAddressSpace -Location $Location
+Add-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $VirtualNetwork -AddressPrefix $SubnetIPRange -NetworkSecurityGroupId $NetworkSecurityGroup.Id
+
+$VirtualNetwork = Set-AzVirtualNetwork -VirtualNetwork $VirtualNetwork
+$Subnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $VirtualNetwork
+
+
+#Step 6: Create Azure Public Address
+$PublicIP = New-AzPublicIpAddress -Name $PublicIPName -ResourceGroupName $ResourceGroupName -Location $Location -AlLocationMethod Static -DomainNameLabel $VMName.ToLower()
+#Setting up the DNS Name
+#$PublicIP.DnsSettings.Fqdn = $FQDN
+
+#Step 7: Create Network Interface Card 
+$NIC = New-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Subnet.Id -PublicIpAddressId $PublicIP.Id -PrivateIpAddress $DomainControllerIP #-NetworkSecurityGroupId $NetworkSecurityGroup.Id
+
+<# Optional : Step 8: Get Virtual Machine publisher, Image Offer, Sku and Image
+$ImagePublisherName = Get-AzVMImagePublisher -Location $Location | Where-Object -FilterScript { $_.PublisherName -eq "MicrosoftWindowsDesktop"}
+$ImageOffer = Get-AzVMImageOffer -Location $Location -publisher $ImagePublisherName.PublisherName | Where-Object -FilterScript { $_.Offer  -eq "Windows-11"}
+$ImageSku = Get-AzVMImageSku -Location  $Location -publisher $ImagePublisherName.PublisherName -offer $ImageOffer.Offer | Where-Object -FilterScript { $_.Skus  -eq "win11-21h2-pro"}
+$image = Get-AzVMImage -Location  $Location -publisher $ImagePublisherName.PublisherName -offer $ImageOffer.Offer -sku $ImageSku.Skus | Sort-Object -Property Version -Descending | Select-Object -First 1
+#>
+
+# Step 9: Create a virtual machine configuration file (As a Spot Intance)
+$VMConfig = New-AzVMConfig -VMName $VMName -VMSize $VMSize -Priority "Spot" -MaxPrice -1
+
+Add-AzVMNetworkInterface -VM $VMConfig -Id $NIC.Id
+
+# Set VM operating system parameters
+Set-AzVMOperatingSystem -VM $VMConfig -Windows -ComputerName $VMName -Credential $Credential
+
+# Set boot diagnostic storage account
+Set-AzVMBootDiagnostic -Enable -ResourceGroupName $ResourceGroupName -VM $VMConfig -StorageAccountName $StorageAccountName    
+
+# The uncommented lines below replace Step #8 : Set virtual machine source image
+Set-AzVMSourceImage -VM $VMConfig -PublisherName $ImagePublisherName -Offer $ImageOffer -Skus $ImageSku -Version 'latest'
+
+# Set OsDisk configuration
+Set-AzVMOSDisk -VM $VMConfig -Name $OSDiskName -DiskSizeInGB $OSDiskSize -StorageAccountType $OSDiskType -CreateOption fromImage
+
+#region Adding Data Disk
+<#
+$VMDataDisk01Config = New-AzDiskConfig -SkuName Standard_LRS -Location $Location -CreateOption Empty -DiskSizeGB 512
+$VMDataDisk01 = New-AzDisk -DiskName $DataDiskName -Disk $VMDataDisk01Config -ResourceGroupName $ResourceGroupName
+$VM = Add-AzVMDataDisk -VM $VMConfig -Name $DataDiskName -CreateOption Attach -ManagedDiskId $VMDataDisk01.Id -Lun 0
+#>
+#endregion
+
+#Step 10: Create Azure Virtual Machine
+New-AzVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VMConfig #-DisableBginfoExtension
+
+$VM = Get-AzVM -ResourceGroup $ResourceGroupName -Name $VMName
+#region JIT Access Management
+#region Enabling JIT Access
+$NewJitPolicy = (@{
+        id = $VM.Id
+        ports = (@{
+                number = $RDPPort;
+                protocol = "*";
+                allowedSourceAddressPrefix = "*";
+                maxRequestAccessDuration = "PT$($JitPolicyTimeInHours)H"
+            })   
+    })
+
+
+Write-Host "Get Existing JIT Policy. You can Ignore the error if not found."
+$ExistingJITPolicy = (Get-AzJitNetworkAccessPolicy -ResourceGroupName $ResourceGroupName -Location $Location -Name $JitPolicyName -ErrorAction Ignore).VirtualMachines
+$UpdatedJITPolicy = $ExistingJITPolicy.Where{ $_.id -ne "$($VM.Id)" } # Exclude existing policy for $VMName
+$UpdatedJITPolicy.Add($NewJitPolicy)
+	
+# Enable Access to the VM including management Port, and Time Range in Hours
+Write-Host "Enabling Just in Time VM Access Policy for ($VMName) on port number $RDPPort for maximum $JitPolicyTimeInHours hours..."
+$null = Set-AzJitNetworkAccessPolicy -VirtualMachine $UpdatedJITPolicy -ResourceGroupName $ResourceGroupName -Location $Location -Name $JitPolicyName -Kind "Basic"
+#endregion
+
+#region Requesting Temporary Access : 3 hours
+$JitPolicy = (@{
+        id = $VM.Id
+        ports = (@{
+                number = $RDPPort;
+                endTimeUtc = (Get-Date).AddHours(3).ToUniversalTime()
+                allowedSourceAddressPrefix = @($MyPublicIP) 
+            })
+    })
+$ActivationVM = @($JitPolicy)
+Write-Host "Requesting Temporary Acces via Just in Time for ($VMName) on port number $RDPPort for maximum $JitPolicyTimeInHours hours..."
+Start-AzJitNetworkAccessPolicy -ResourceGroupName $($VM.ResourceGroupName) -Location $VM.Location -Name $JitPolicyName -VirtualMachine $ActivationVM
+#endregion
+
+#endregion
+
+#region Enabling auto-shutdown at 11:00 PM in the user time zome
+$SubscriptionId = ($VM.Id).Split('/')[2]
+$ScheduledShutdownResourceId = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/microsoft.devtestlab/schedules/shutdown-computevm-$VMName"
+$Properties = @{}
+$Properties.Add('status', 'Enabled')
+$Properties.Add('taskType', 'ComputeVmShutdownTask')
+$Properties.Add('dailyRecurrence', @{'time' = "2300" })
+$Properties.Add('timeZoneId', (Get-TimeZone).Id)
+$Properties.Add('targetResourceId', $VM.Id)
+New-AzResource -Location $location -ResourceId $ScheduledShutdownResourceId -Properties $Properties -Force
+#endregion
+
+#Step 11: Start Azure Virtual Machine
+Start-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName
+
+#region Setting up the Key Vault for HTTPS
 
 #Create an Azure Key Vault
 $Vault = New-AzKeyVault -VaultName $KeyVaultName -ResourceGroup $ResourceGroupName -Location $location -EnabledForDeployment
@@ -25,8 +321,7 @@ $Vault = New-AzKeyVault -VaultName $KeyVaultName -ResourceGroup $ResourceGroupNa
 $Policy = New-AzKeyVaultCertificatePolicy -SubjectName "CN=$FQDN" -SecretContentType "application/x-pkcs12" -IssuerName Self -ValidityInMonths 12
 Add-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName -CertificatePolicy $Policy
 
-While (-not((Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName).SecretId)) 
-{
+While (-not((Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName).SecretId)) {
     Write-Host "Sleeping 30 seconds ..."
     Start-Sleep 30
 }
@@ -35,18 +330,33 @@ While (-not((Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $Certifica
 #$CertUrl=(Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName).SecretId
 #$VaultId=(Get-AzKeyVault -ResourceGroupName $ResourceGroupName -VaultName $KeyVaultName).ResourceId
 
-$CertUrl=(Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $CertificateName).id
-$VaultId=$Vault.ResourceId
+$CertificateThumbprint = (Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName).Certificate.Thumbprint
+$CertUrl = (Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $CertificateName).id
+$VaultId = $Vault.ResourceId
 $VM = Add-AzVMSecret -VM $VM -SourceVaultId $VaultId -CertificateStore "My" -CertificateUrl $CertUrl
 $VM | Update-AzVM -Verbose
 #endregion 
 
-#Configure IIS manually to set the HTTPS binding
+#region Setting up the DSC extension
 
-#Get the Public IP address dynamically
-#$PublicIP = Get-AzPublicIpAddress -ResourceGroupName $ResourceGroupName | Where-Object { $_.IpConfiguration.Id -like "*$VMName*" } | Select-Object -First 1
+# Publishing DSC Configuration for AutomatedLab via Hyper-V (Nested Virtualization)
+Publish-AzVMDscConfiguration $DSCFilePath -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -Force -Verbose
 
-#Browsing to the IIS Web Site via HTTPS
-#Start-Process "https://$FQDN"
+try
+{
+    Set-AzVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -ArchiveBlobName "$DSCFileName.zip" -ArchiveStorageAccountName $StorageAccountName -ConfigurationName $ConfigurationName -ConfigurationArgument @{CertificateThumbprint=$CertificateThumbprint}  -Version "2.80" -Location $Location -AutoUpdate -Verbose #-ErrorAction Ignore
+} catch {}
+$VM | Update-AzVM -Verbose
+#endregion
 
+
+# Adding Credentials to the Credential Manager (and escaping the password)
+Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "cmdkey /generic:$FQDN /user:$($Credential.UserName) /pass:$($Credential.GetNetworkCredential().Password -replace "(\W)", '^$1')" -Wait
+
+Start-Sleep -Seconds 15
+
+#Step 13: Start RDP Session
+#mstsc /v $PublicIP.IpAddress
+Start-Process -FilePath "https://$FQDN"
 mstsc /v $FQDN
+Write-Host -Object "Your RDP credentials (login/password) are $($Credential.UserName)/$($Credential.GetNetworkCredential().Password)" -ForegroundColor Green
