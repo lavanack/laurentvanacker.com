c2db777a3b0d172dfc625a89d3929df82e737d46
Laurent VAN ACKER <laurent.vanacker@free.fr>
2023-06-23 17:40:45 +0200

    Major updates - Switching to function - Writing to verbose for non-deleted VM cred - -WhatIf support

--- Azure/Misc/Remove-Credential.ps1
+++ Azure/Misc/Remove-Credential.ps1
@@ -1,21 +1,48 @@
-﻿Clear-Host
-$AzureCredentials = cmdkey /list | Select-string -Pattern "=(TERMSRV/)?((.*)\.(.*)\.cloudapp\.azure\.com)" -AllMatches
-if ($AzureCredentials.Matches)
-{
-    $AzureCredentials.Matches | ForEach-Object -Process { 
-        $TERMSRV = $($_.Groups[1].Value)
-        $DNSName = $_.Groups[2].Value
-        $VMName = $_.Groups[3].Value
-        $Location = $_.Groups[4].Value
-        $AzVM = Get-AzVM -Name $VMName 
-        if (($AzVM) -and ($AzVM.Location -eq $Location))
-        {
-            Write-Host -Object "$VMName Azure VM exists" -ForegroundColor Green
-        }
-        else
-        {
-            Write-Warning -Message "$VMName Azure VM doesn't exist. The related credentials will be removed from the Windows Credential Manager"
-            Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "cmdkey /delete:$TERMSRV$DNSName" -Wait
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5
+
+function Remove-Credential {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param
+    (
+    )
+
+    Clear-Host
+    $AzureCredentials = cmdkey /list | Select-string -Pattern "=(TERMSRV/)?((.*)\.(.*)\.cloudapp\.azure\.com)" -AllMatches
+    if ($AzureCredentials.Matches) {
+        $AzureCredentials.Matches | ForEach-Object -Process { 
+            $TERMSRV = $($_.Groups[1].Value)
+            $DNSName = $_.Groups[2].Value
+            $VMName = $_.Groups[3].Value
+            $Location = $_.Groups[4].Value
+            $AzVM = Get-AzVM -Name $VMName 
+            if (($AzVM) -and ($AzVM.Location -eq $Location)) {
+                Write-Verbose -Message "$VMName ($DNSName) Azure VM exists"
+            }
+            else {
+                If ($pscmdlet.ShouldProcess($DNSName, 'Removing Credential from the Windows Credential Manager')) {
+                    Write-Warning -Message "$VMName ($DNSName) Azure VM doesn't exist. The related credentials will be removed from the Windows Credential Manager"
+                    Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "cmdkey /delete:$TERMSRV$DNSName" -Wait
+                }
+            }
         }
     }
 }
+
+Remove-Credential #-Verbose #-WhatIf
