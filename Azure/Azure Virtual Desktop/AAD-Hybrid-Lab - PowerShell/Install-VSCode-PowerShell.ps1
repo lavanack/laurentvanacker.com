911ab214308803e565c5ae03b6ae33d866462689
Laurent VAN ACKER <laurent.vanacker@free.fr>
2023-09-26 16:25:16 +0200

    Initial Commit

---
+++ Azure/Azure Virtual Desktop/AAD-Hybrid-Lab - PowerShell/Install-VSCode-PowerShell.ps1
@@ -0,0 +1,2 @@
+﻿Invoke-Expression -Command "& { $(Invoke-RestMethod https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1) }"
+Invoke-Expression -Command "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
