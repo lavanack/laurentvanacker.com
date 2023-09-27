a69c55be872fabe44ec1a250e3e48a7ffbc11a96
Laurent VAN ACKER <48048478+lavanack@users.noreply.github.com>
2023-09-27 07:40:40 +0200

    Delete Azure/Azure Virtual Desktop/AAD-Hybrid-Lab - PowerShell/Install-VSCode-PowerShell.ps1

--- Azure/Azure Virtual Desktop/AAD-Hybrid-Lab - PowerShell/Install-VSCode-PowerShell.ps1
+++
@@ -1,2 +0,0 @@
-﻿Invoke-Expression -Command "& { $(Invoke-RestMethod https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1) }"
-Invoke-Expression -Command "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
