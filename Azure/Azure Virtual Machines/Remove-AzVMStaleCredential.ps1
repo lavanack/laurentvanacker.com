0d9f4972f9713254423829e465378b3e012ce63e
Laurent VAN ACKER <laurent.vanacker@free.fr>
2023-10-12 10:33:54 +0200

    Adding named capture groups

--- Azure/Azure Virtual Machines/Remove-AzVMStaleCredential.ps1
+++ Azure/Azure Virtual Machines/Remove-AzVMStaleCredential.ps1
@@ -17,34 +17,30 @@ of the Sample Code.
 #>
 #requires -Version 5
 
-function Remove-AzVMStaleCredential{
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact='High')]
+function Remove-AzVMStaleCredential {
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     Param
     (
     )
-    if (-not(Get-AzContext))
-    {
+    if (-not(Get-AzContext)) {
         Write-Verbose -Message "No account connection to Azure detected ..."
         Connect-AzAccount
         Get-AzSubscription | Out-GridView -OutputMode Single -Title "Select your Azure Subscription" | Select-AzSubscription
     }
     Clear-Host
-    $AzureCredentials = cmdkey /list | Select-string -Pattern "=(TERMSRV/)?((.*)\.(.*)\.cloudapp\.azure\.com)" -AllMatches
-    if ($AzureCredentials.Matches)
-    {
+    $AzureCredentials = cmdkey /list | Select-string -Pattern "=(?<termsrv>TERMSRV/)?(?<dnsname>(?<vmname>.*)\.(?<location>.*)\.cloudapp\.azure\.com)" -AllMatches
+    if ($AzureCredentials.Matches) {
         $AzureCredentials.Matches | ForEach-Object -Process { 
-            $TERMSRV = $($_.Groups[1].Value)
-            $DNSName = $_.Groups[2].Value
-            $VMName = $_.Groups[3].Value
-            $Location = $_.Groups[4].Value
+            $TERMSRV = $($_.Groups['termsrv'].Value)
+            $DNSName = $_.Groups['dnsname'].Value
+            $VMName = $_.Groups['vmname'].Value
+            $Location = $_.Groups['location'].Value
             $AzVM = Get-AzVM -Name $VMName 
-            if (($AzVM) -and ($AzVM.Location -eq $Location))
-            {
+            if (($AzVM) -and ($AzVM.Location -eq $Location)) {
                 Write-Verbose -Message "$VMName ($DNSName) Azure VM exists. The related credentials will stay into the Windows Credential Manager"
             }
-            else
-            {
-				If ($pscmdlet.ShouldProcess($DNSName, 'Removing Credential from the Windows Credential Manager')) {
+            else {
+                If ($pscmdlet.ShouldProcess($DNSName, 'Removing Credential from the Windows Credential Manager')) {
                     Write-Warning -Message "$VMName ($DNSName) Azure VM doesn't exist. The related credentials will be removed from the Windows Credential Manager"
                     Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "cmdkey /delete:$TERMSRV$DNSName" -Wait
                 }
