29e67f3073b4c24dcdea7bb358af3ed241440c5d
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-01-12 17:23:43 +0100

    Initial commit

---
+++ Windows Powershell/Windows/Misc/Get-ShortcutTarget.ps1
@@ -0,0 +1,23 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+
+Clear-Host
+#For Reparse Point
+Get-ChildItem -Path \ -Attributes ReparsePoint -ErrorAction Ignore | Select-Object -Property Name, Target, @{Name="Type"; Expression= {"ReparsePoint"}}
+#For Shorcut
+Get-ChildItem -Path \ -Filter "*.lnk" -ErrorAction Ignore | Select-Object -Property Name, @{Name="Target"; Expression= {$WScript = New-Object -ComObject WScript.Shell; $WScript.CreateShortcut($_.FullName).TargetPath}}, @{Name="Type"; Expression= {"Link"}}
