0c8d4aa82dc2512229cd1dc4dffdc7aab4dd63e9
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-03-04 16:40:06 +0100

    Minor update

--- Azure/Desired State Configuration/Azure Automanage Machine Configuration/3 - AzureAutomanageMachineConfiguration.ps1
+++ Azure/Desired State Configuration/Azure Automanage Machine Configuration/3 - AzureAutomanageMachineConfiguration.ps1
@@ -8,8 +8,8 @@
 #>
 <#
 #Cleaning up previous tests 
-Get-AzResourceGroup -Name dscagc* | Select-Object -Property @{Name="Scope"; Expression={$_.ResourceID}} | Get-AzPolicyRemediation | Remove-AzPolicyRemediation -AllowStop -AsJob -Verbose | Wait-Job
-Get-AzResourceGroup -Name dscagc* | Select-Object -Property @{Name="Scope"; Expression={$_.ResourceID}} | Get-AzPolicyAssignment  | Where-Object -FilterScript { $_.ResourceGroupName -like '*dscagc*' } | Remove-AzPolicyAssignment -Verbose #-Whatif
+Get-AzResourceGroup -Name rg-dsc-amc* | Select-Object -Property @{Name="Scope"; Expression={$_.ResourceID}} | Get-AzPolicyRemediation | Remove-AzPolicyRemediation -AllowStop -AsJob -Verbose | Wait-Job
+Get-AzResourceGroup -Name rg-dsc-amc* | Select-Object -Property @{Name="Scope"; Expression={$_.ResourceID}} | Get-AzPolicyAssignment  | Where-Object -FilterScript { $_.ResourceGroupName -like 'rg-dsc-amc*' } | Remove-AzPolicyAssignment -Verbose #-Whatif
 Get-AzPolicyDefinition | Where-Object -filterScript {$_.Properties.metadata.category -eq "Guest Configuration" -and $_.Properties.DisplayName -like "*CreateAdminUserDSCConfiguration*"} | Remove-AzPolicyDefinition -Verbose -Force #-WhatIf
 #>
 
