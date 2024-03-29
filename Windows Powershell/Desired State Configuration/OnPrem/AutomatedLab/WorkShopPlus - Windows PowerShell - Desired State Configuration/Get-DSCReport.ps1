daf4d4390a268c9e171f2cc21811e2f94835a3f2
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-03-29 22:09:46 +0100

    Initial commit

---
+++ Windows Powershell/Desired State Configuration/OnPrem/AutomatedLab/WorkShopPlus - Windows PowerShell - Desired State Configuration/Get-DSCReport.ps1
@@ -0,0 +1,18 @@
+﻿#From https://docs.microsoft.com/en-us/powershell/scripting/dsc/pull-server/reportserver?view=powershell-7
+function Get-DSCReport
+{
+    param
+    (
+        $AgentId = "$((glcm).AgentId)", 
+        $serviceURL = "$((glcm).ReportManagers.ServerUrl)"
+    )
+
+    $requestUri = "$serviceURL/Nodes(AgentId= '$AgentId')/Reports"
+    $request = Invoke-WebRequest -Uri $requestUri  -ContentType "application/json;odata=minimalmetadata;streaming=true;charset=utf-8" `
+               -UseBasicParsing -Headers @{Accept = "application/json";ProtocolVersion = "2.0"} `
+               -ErrorAction SilentlyContinue -ErrorVariable ev
+    $object = ConvertFrom-Json $request.content
+    return $object.value
+}
+
+Get-DSCReport
