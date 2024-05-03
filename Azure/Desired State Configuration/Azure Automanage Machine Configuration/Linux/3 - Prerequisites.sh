be8d823a9b8f51bcf5bb21dee77c3c8051879d23
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-05-03 18:08:40 +0200

    Update

--- Azure/Desired State Configuration/Azure Automanage Machine Configuration/Linux/3 - Prerequisites.sh
+++ Azure/Desired State Configuration/Azure Automanage Machine Configuration/Linux/3 - Prerequisites.sh
@@ -4,6 +4,6 @@ wget https://aka.ms/install-powershell.sh; sudo bash install-powershell.sh; rm i
 #https://github.com/orgs/PowerShell/discussions/15310
 sudo pwsh -Command 'Install-Module -Name PSWSMan -Scope AllUsers -Force'
 sudo pwsh -Command 'Install-WSMan'
-sudo pwsh -Command 'Install-Module -Name Az.Accounts, Az.Compute, Az.PolicyInsights, Az.Resources, Az.Ssh, Az.Storage -Scope AllUsers -Force'
+sudo pwsh -Command 'Install-Module -Name Az.Accounts, Az.Compute, Az.PolicyInsights, Az.Resources, Az.Ssh, Az.Storage, GuestConfiguration -Scope AllUsers -Force'
 
 sudo pwsh -File './4 - AzureAutomanageMachineConfiguration.ps1'
