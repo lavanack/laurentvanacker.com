db09bd18b76899764d09b36fb8193aa5ab0392aa
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-05-07 14:36:46 +0200

    Update

--- Azure/Azure Key Vault/Azure Key Vault for Disk Encryption with Generated Customer Managed Key.ps1
+++ Azure/Azure Key Vault/Azure Key Vault for Disk Encryption with Generated Customer Managed Key.ps1
@@ -15,7 +15,7 @@ Our suppliers from and against any claims or lawsuits, including
 attorneys' fees, that arise or result from the use or distribution
 of the Sample Code.
 #>
-#requires -Version 5 -Modules Az.Compute, Az.Network, Az.Storage, Az.Resources, Az.KeyVault
+#requires -Version 5 -Modules Az.Accounts, Az.Compute, Az.KeyVault, Az.Network, Az.Resources, Az.Storage, Az.Security
 
 [CmdletBinding()]
 param
