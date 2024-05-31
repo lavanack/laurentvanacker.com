a75757d0385eec4566892c92afce92704bc29712
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-05-31 15:10:05 +0200

    Initial commit

---
+++ Azure/Azure Virtual Machine/Get-AzVMBootDiagnosticsData.ps1
@@ -0,0 +1,89 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, th
+at arise or result from the use or distribution
+of the Sample Code.
+#>
+
+#requires -Version 5 -Modules Az.Accounts, Az.Compute
+
+#region Function definitions
+function Get-AzVMBootDiagnosticsDataSetting {
+    [CmdletBinding()]
+    Param()
+    Get-AzVM | ForEach-Object -Process {
+        $VMName = $_.Name
+        $_.DiagnosticsProfile.BootDiagnostics | Select-Object -Property @{Name = "VMName"; Expression = { $VMName } }, *
+    }
+}
+
+function Get-AzVMBootDiagnosticsDataItem {
+    [CmdletBinding()]
+    Param(
+        [string] $LocalPath = $(Join-Path -Path $env:TEMP -ChildPath "BootDiagnostics")
+    )
+    $null = New-Item -Path $LocalPath -ItemType Directory -Force
+    Get-AzVM | Where-Object -FilterScript { ($_.OSProfile.WindowsConfiguration) -and ($_.DiagnosticsProfile.BootDiagnostics.Enabled) } | Get-AzVMBootDiagnosticsData -Windows -LocalPath $LocalPath
+}
+
+function Get-AzVMBootDiagnosticsDataBlobUri {
+    [CmdletBinding()]
+    Param(
+    )
+    #region Azure Context
+    # Log in first with Connect-AzAccount if not using Cloud Shell
+
+    $azContext = Get-AzContext
+    $SubcriptionID = $azContext.Subscription.Id
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+    $authHeader = @{
+        'Content-Type'  = 'application/json'
+        'Authorization' = 'Bearer ' + $token.AccessToken
+    }
+    #endregion
+    $AzVMWithBootDiagnostics = Get-AzVM | Where-Object -FilterScript { ($_.DiagnosticsProfile.BootDiagnostics.Enabled) }
+    $AzVMBootDiagnosticsDataBlobUri = foreach ($CurrentAzVM in $AzVMWithBootDiagnostics) {
+        Write-Verbose -Message "Processing '$($CurrentAzVM.Name)' Azure VM"
+        $URI = "https://management.azure.com/subscriptions/$SubcriptionID/resourceGroups/$($CurrentAzVM.ResourceGroupName)/providers/Microsoft.Compute/virtualMachines/$($CurrentAzVM.Name)/retrieveBootDiagnosticsData?api-version=2024-03-01"
+        Write-Verbose -Message "`$URI: $URI"
+        try {
+            # Invoke the REST API
+            $Response = Invoke-RestMethod -Method POST -Headers $authHeader -ContentType "application/json" -Uri $URI -ErrorVariable ResponseError
+        }
+        catch [System.Net.WebException] {   
+            # Dig into the exception to get the Response details.
+            # Note that value__ is not a typo.
+            Write-Warning -Message "StatusCode: $($_.Exception.Response.StatusCode.value__ )"
+            Write-Warning -Message "StatusDescription: $($_.Exception.Response.StatusDescription)"
+            $respStream = $_.Exception.Response.GetResponseStream()
+            $reader = New-Object System.IO.StreamReader($respStream)
+            $Response = $reader.ReadToEnd() | ConvertFrom-Json
+            if (-not([string]::IsNullOrEmpty($Response.message))) {
+                Write-Warning -Message $Response.message
+            }
+        }
+        finally {
+        }
+        $Response | Select-Object -Property @{Name = "VMName"; Expression = { $CurrentAzVM.Name } }, *
+    }
+    $AzVMBootDiagnosticsDataBlobUri 
+}
+#endregion
+
+#Get-AzVMBootDiagnosticsDataSetting -Verbose
+#Get-AzVMBootDiagnosticsDataItem -Verbose
+Get-AzVMBootDiagnosticsDataBlobUri -Verbose | Format-List -Property * -Force
