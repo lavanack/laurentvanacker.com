d43abb7aa34e72164d26879456efe48459fc98e7
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-06-26 13:01:47 +0200

    Update

--- Azure/Azure Virtual Desktop/Setup/Set-FSLogix.ps1
+++ Azure/Azure Virtual Desktop/Setup/Set-FSLogix.ps1
@@ -1,23 +1,4 @@
-﻿<#
-This Sample Code is provided for the purpose of illustration only
-and is not intended to be used in a production environment.  THIS
-SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
-WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
-LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
-FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
-right to use and modify the Sample Code and to reproduce and distribute
-the object code form of the Sample Code, provided that You agree:
-(i) to not use Our name, logo, or trademarks to market Your software
-product in which the Sample Code is embedded; (ii) to include a valid
-copyright notice on Your software product in which the Sample Code is
-embedded; and (iii) to indemnify, hold harmless, and defend Us and
-Our suppliers from and against any claims or lawsuits, including
-attorneys' fees, that arise or result from the use or distribution
-of the Sample Code.
-#>
-#requires -Version 5
-
-param (
+﻿param (
     [ValidateSet("Enabled", "Disabled")]
     [String] $Status = "Disabled"
 )
@@ -28,7 +9,8 @@ if ($Status -eq "Disabled") {
 else {
     $RegistryValue = 1
 }
-foreach ($CurrentHostPool in Get-AzWvdHostPool) {
+foreach ($CurrentHostPool in Get-AzWvdHostPool)
+{
     $CurrentHostPoolResourceGroupName = ((Get-AzWvdHostPool).Id -split "/")[4]
     $SessionHosts = Get-AzWvdSessionHost -HostpoolName $CurrentHostPool.Name -ResourceGroupName $CurrentHostPoolResourceGroupName
     $SessionHostNames = $SessionHosts.ResourceId -replace ".*/"
@@ -49,6 +31,5 @@ foreach ($CurrentHostPool in Get-AzWvdHostPool) {
         Restart-AzVM -Name $CurrentSessionHostName -ResourceGroupName $CurrentHostPoolResourceGroupName -Confirm:$false -AsJob
     }
     $Jobs | Wait-Job | Out-Null
-    $Jobs | Remove-Job -Force
-}
-#endregion
+    $Jobs | Remove-Job -Force}
+    #endregion
