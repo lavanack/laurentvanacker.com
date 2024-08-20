efff8906485555d35d4cc8eccbc8b5a863141c3c
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-08-20 11:06:38 +0200

    Minor update

--- Azure/Azure Virtual Machine/Get-AzRunningVMJITAccess.ps1
+++ Azure/Azure Virtual Machine/Get-AzRunningVMJITAccess.ps1
@@ -1,10 +1,28 @@
+<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+
 Clear-Host
 
 #region Defining variables 
 $RDPPort = 3389
 $JitPolicyTimeInHours = 3
 $JitPolicyName = "Default"
-$MyPublicIp = (Invoke-WebRequest -uri "https://ipv4.seeip.org").Content
+$MyPublicIp = (Invoke-RestMethod -Uri http://ip-api.com/json/?fields=query).query
 $RunningVM = Get-AzVM -Status | Where-Object -FilterScript { $_.PowerState -match "running" }
 #endregion
 
