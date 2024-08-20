6882251a14e9a0884e0d982aa24191d49255aae4
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-08-20 11:10:13 +0200

    Minor update

--- Azure/Azure Key Vault/Azure Key Vault for VM Credentials.ps1
+++ Azure/Azure Key Vault/Azure Key Vault for VM Credentials.ps1
@@ -15,8 +15,7 @@ Our suppliers from and against any claims or lawsuits, including
 attorneys' fees, that arise or result from the use or distribution
 of the Sample Code.
 #>
-#requires -Version 5 -Modules Az.Compute, Az.Network, Az.Storage, Az.Resources
-#requires -Modules @{ ModuleName="Az.KeyVault"; ModuleVersion="6.0.1" }
+#requires -Version 5 -Modules Az.Compute, Az.Network, Az.Storage, Az.Resources, @{ ModuleName="Az.KeyVault"; ModuleVersion="6.0.1" }
 
 [CmdletBinding()]
 param
