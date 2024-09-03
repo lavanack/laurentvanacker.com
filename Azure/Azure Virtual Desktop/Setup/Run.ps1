61a7ffa4faf761612688f198474d1a0943ecd146
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-09-03 14:36:04 +0200

    Initial commit

---
+++ Azure/Azure Virtual Desktop/Setup/Run.ps1
@@ -0,0 +1,75 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+
+[CmdletBinding()]
+Param (
+)
+
+Clear-Host
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir
+
+$Global:MaximumFunctionCount = 32768
+try {
+    while (Stop-Transcript) {
+    } 
+}
+catch {
+}
+#Get-Job | Remove-Job -Force
+Get-Job | Where-Object -FilterScript { $_.PSJobTypeName -eq "ThreadJob" } | Remove-Job -Force -Verbose
+$null = Remove-Module -Name PSAzureVirtualDesktop -Force -ErrorAction Ignore
+Import-Module -Name PSAzureVirtualDesktop -Force
+
+<#
+#>
+Connect-MgGraph -NoWelcome
+try { 
+    $null = Get-AzAccessToken -ErrorAction Stop
+}
+catch {
+    Connect-AzAccount
+    Get-AzSubscription | Out-GridView -OutputMode Single | Select-AzSubscription
+}
+try {
+    Get-ChildItem -Path ~\Documents\ -Filter HostPool_* -Directory | Remove-Item -Force -Recurse -ErrorAction Stop
+}
+catch {
+    Stop-Process -Name notepad, powershell* -Force -ErrorAction Ignore
+}
+Get-AzResourceGroup | Where-Object -FilterScript { $_.ResourceGroupName -match 'poc|kv|amc|ama' } | Remove-AzResourceGroup -AsJob -Force -Verbose
+Get-MgBetaGroup -Filter "DisplayName eq 'No-MFA Users'" | ForEach-Object -Process { Remove-MgBetaGroup -GroupId $_.Id -Verbose }
+
+Get-MgBetaIdentityConditionalAccessPolicy -Filter "displayName eq '[AVD] Require multifactor authentication for all users'" | ForEach-Object -Process { Remove-MgBetaIdentityConditionalAccessPolicy -ConditionalAccessPolicyId $_.Id -Verbose }
+& '.\New-AzAvdHostPoolSetup.ps1' -Verbose -AsJob
+
+<#
+$storageAccounts = Get-AzStorageAccount
+# Loop through each storage account
+foreach ($storageAccount in $storageAccounts)
+{
+    # Get the list of file shares in the storage account
+    $AzStorageShare = Get-AzStorageShare -Context $storageAccount.Context -ErrorAction Ignore
+    $profilesStorageShare = $AzStorageShare | Where-Object  -FilterScript {$_.Name -eq "profiles"}
+    if ($null -ne $profilesStorageShare) {
+        start $("\\{0}.file.{1}\{2}" -f $profilesStorageShare.context.StorageAccountName, ($profilesStorageShare.context.EndPointSuffix -replace "/"), $profilesStorageShare.Name)
+    }
+}
+#> 
