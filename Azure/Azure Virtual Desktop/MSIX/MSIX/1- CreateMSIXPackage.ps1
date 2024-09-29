253fca5767c6efb09f069ebff77e182f65b94efd
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-09-29 13:28:45 +0200

    Moving away from winget

--- Azure/Azure Virtual Desktop/MSIX/MSIX/1- CreateMSIXPackage.ps1
+++ Azure/Azure Virtual Desktop/MSIX/MSIX/1- CreateMSIXPackage.ps1
@@ -8,26 +8,25 @@ $CurrentDir = Split-Path -Path $CurrentScript -Parent
 
 Set-Location -Path $CurrentDir
 
+$MSIXPackageURL = "https://download.microsoft.com/download/d/0/0/d0043667-b1db-4060-9c82-eaee1fa619e8/493b543c21624db8832da8791ebf98f3.msixbundle"
+#Installing MSIX Packaging Tool
+Invoke-WebRequest -Uri $MSIXPackageURL -OutFile "C:\MSIX\MsixPackagingTool.msixbundle"
+Add-AppPackage -Path "C:\MSIX\MSIXPackagingTool.msixbundle"
 
-Set-WinUserLanguageList -LanguageList fr-fr -Force
-#region Installing winget via the WingetTools Powershell module
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name WingetTools -Force -Verbose
-Install-WinGet -Preview -PassThru -Verbose
-#endregion
+#Installing PSFTooling Tool
+$PsfToolPackageURL = "https://www.tmurgent.com/AppV/Tools/PsfTooling/PsfTooling-6.3.0.0-x64.msix"
+Invoke-WebRequest -Uri $PsfToolPackageURL -OutFile "C:\MSIX\PsfTooling-x64.msix"
+Add-AppPackage -Path "C:\MSIX\PsfTooling-x64.msix" 
 
-$CommandLine = 'winget install "MSIX Packaging Tool" --source msstore --accept-source-agreements --accept-package-agreements'
-Start-Process -FilePath $env:ComSpec -ArgumentList "/c", $CommandLine -Wait
 
 # Install only the PowerShell module to have VHD Management cmdlets
-if ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell).State -ne 'Enabled')
-{
+if ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell).State -ne 'Enabled') {
     #Manage MSIX app attach
     # Install only the PowerShell module to have VHD Management cmdlets
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell -All
     Restart-Computer -Force
 }
-Set-Location $CurrentDir
+
 #10 years from now
 $ExpirationDate = (Get-Date).AddYears(10)
 $ClearTextPassword = 'P@ssw0rd'
@@ -35,43 +34,43 @@ $ClearTextPassword = 'P@ssw0rd'
 $PFXFilePath = Join-Path -Path $CurrentDir -ChildPath MSIXDigitalSignature.pfx
 $SecurePassword = ConvertTo-SecureString -String $ClearTextPassword -AsPlainText -Force
 #Remove any previously existing certificate
-Get-ChildItem Cert:\LocalMachine -Recurse | Where-Object -FilterScript {$_.Subject -eq "CN=Contoso Software, O=Contoso Corporation, C=US"} | Remove-Item -Verbose -Force 
+Get-ChildItem Cert:\LocalMachine -Recurse | Where-Object -FilterScript { $_.Subject -eq "CN=Contoso Software, O=Contoso Corporation, C=US" } | Remove-Item -Verbose -Force 
 
 #If we don't already have a PFX file we can use ==> Generating a Self-signed certificate expiring in 10 years
-if (-not(Test-Path -Path $PFXFilePath))
-{
+if (-not(Test-Path -Path $PFXFilePath)) {
     #$cert = New-SelfSignedCertificate -Type Custom -Subject "CN=Contoso Software, O=Contoso Corporation, C=US" -KeyUsage DigitalSignature -FriendlyName "'MSIX Code Signing Certificate" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") -NotAfter $ExpirationDate
     $cert = New-SelfSignedCertificate -Type Custom -Subject "CN=Contoso Software, O=Contoso Corporation, C=US" -KeyUsage DigitalSignature -FriendlyName "'MSIX Code Signing Certificate" -KeyAlgorithm RSA -KeyLength 2048 -CertStoreLocation "cert:\LocalMachine\My" -NotAfter $ExpirationDate
     $cert | Export-PfxCertificate -FilePath $PFXFilePath -Password $SecurePassword
     #Remove the newly generated certificate
-    Get-ChildItem Cert:\LocalMachine -Recurse | Where-Object -FilterScript {$_.Subject -eq "CN=Contoso Software, O=Contoso Corporation, C=US"} | Remove-Item -Verbose -Force 
+    Get-ChildItem Cert:\LocalMachine -Recurse | Where-Object -FilterScript { $_.Subject -eq "CN=Contoso Software, O=Contoso Corporation, C=US" } | Remove-Item -Verbose -Force 
 }
 
 #Getting notepad++ latest version if possible
 $HTMLResponse = Invoke-WebRequest -Uri "https://notepad-plus-plus.org/downloads/"
-if ($HTMLResponse.Content -match "Current Version (?<version>\d\.\d\.\d)")
-{
+if ($HTMLResponse.Content -match "Current Version (?<version>\d\.\d(\.\d)?)") {
     #Get the latest version
-    $NotepadPlusPlusVersion=$Matches["Version"]
+    $NotepadPlusPlusVersion = $Matches["Version"]
 }
-else
-{
-    #Update this line to reflect the latest release of Notepad++ 
-    $NotepadPlusPlusVersion="8.4.8"
+else {
+    #Should never be reached but in case: Update this line to reflect the latest release of Notepad++ 
+    $NotepadPlusPlusVersion = "8.7.0"
 }
 #Downloading notepad++ 
 $NotepadPlusPlusUri = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v$NotepadPlusPlusVersion/npp.$NotepadPlusPlusVersion.Installer.x64.exe"
 $Outfile = Join-Path -Path $CurrentDir -ChildPath $(Split-Path -Path $NotepadPlusPlusUri -Leaf)
-If (-not(Test-Path -Path $Outfile))
-{
+If (-not(Test-Path -Path $Outfile)) {
     Write-Verbose -Message "Downloading Notepad++ v$NotepadPlusPlusVersion ..."
     Invoke-WebRequest -Uri $NotepadPlusPlusUri -UseBasicParsing -OutFile $Outfile -Verbose
 }
 
+#To have a X.Y.Z version (Where Z=0) in case of only X.Y version
+if ($NotepadPlusPlusVersion -match "^\d\.\d$") {
+    $NotepadPlusPlusVersion = "{0}.0" -f $NotepadPlusPlusVersion
+}
 
 #Uninstalling any previously installed notepad++ program (if any)
 #https://github.com/MicrosoftDocs/PowerShell-Docs/issues/4619
-Get-Package "notepad*" -ErrorAction Ignore | ForEach-Object -Process { Start-Process $($_.Meta.Attributes["UninstallString"])}
+Get-Package "notepad*" -ErrorAction Ignore | ForEach-Object -Process { Start-Process $($_.Meta.Attributes["UninstallString"]) }
 Get-AppxPackage -Name "notepad*" | Remove-AppxPackage -Verbose
 
 #region : Demonstration via GUI
@@ -80,18 +79,19 @@ Get-AppxPackage -Name "notepad*" | Remove-AppxPackage -Verbose
 #endregion
 
 #Log files
-start $env:LOCALAPPDATA\packages\Microsoft.MsixPackagingTool_8wekyb3d8bbwe\LocalState\DiagOutputDir\
+Start-Process $env:LOCALAPPDATA\Packages\Microsoft.MSIXPackagingTool_8wekyb3d8bbwe\LocalState\DiagOutputDir\
 
-$MSIXFilePath = Get-ChildItem -Path $CurrentDir -Filter *.msix -File
-$MSIXFilePath
+$LatestNotepadMSIXFilePath = Get-ChildItem -Path $CurrentDir -Filter *.msix -File | Where-Object -FilterScript { $_.BaseName -match "notepad" } | Sort-Object -Descending | Select-Object -First 1
+$LatestNotepadMSIXFilePath
 
 #Uninstalling any previously installed notepad++ program (if any)
 #https://github.com/MicrosoftDocs/PowerShell-Docs/issues/4619
 #Get-AppxPackage -Name "notepad*" | Remove-AppxPackage -Verbose
-Get-Package "notepad*" -ErrorAction Ignore | ForEach-Object -Process { Start-Process $($_.Meta.Attributes["UninstallString"])}
+Get-Package "notepad*" -ErrorAction Ignore | ForEach-Object -Process { Start-Process $($_.Meta.Attributes["UninstallString"]) }
 
 #To create a VHD
-$MSIXVHD = Join-Path -Path $CurrentDir -ChildPath notepadplusplus.vhd
+$VHDXFileName = $("notepadplusplus_{0}.vhdx" -f [system.version]::Parse($NotepadPlusPlusVersion))
+$MSIXVHD = Join-Path -Path $CurrentDir -ChildPath $VHDXFileName
 Remove-Item $MSIXVHD -Force -ErrorAction Ignore
 New-VHD -SizeBytes 1GB -Path $MSIXVHD -Dynamic -Confirm:$false
 
@@ -99,7 +99,7 @@ New-VHD -SizeBytes 1GB -Path $MSIXVHD -Dynamic -Confirm:$false
 $vhdObject = Mount-VHD -Path $MSIXVHD -Passthru
 
 #To initialize the VHD, run:
-$disk = Initialize-Disk -Passthru -Number $vhdObject.Number
+$disk = Initialize-Disk -PassThru -Number $vhdObject.Number
 
 #To create a new partition, run:
 $partition = New-Partition -AssignDriveLetter -UseMaximumSize -DiskNumber $disk.Number
@@ -111,13 +111,12 @@ Format-Volume -FileSystem NTFS -Confirm:$false -DriveLetter $partition.DriveLett
 $MSIXMgrUri = "https://aka.ms/msixmgr"
 $Outfile = Join-Path -Path $CurrentDir -ChildPath $(Split-Path -Path $MSIXMgrUri -Leaf)
 $Outfile += ".zip"
-If (-not(Test-Path -Path $Outfile))
-{
+If (-not(Test-Path -Path $Outfile)) {
     Write-Verbose -Message "Downloading MSIX Mgr Tool ..."
     Invoke-WebRequest -Uri $MSIXMgrUri -UseBasicParsing -OutFile $Outfile -Verbose
 }
 Expand-Archive -Path .\msixmgr.zip -Force
 
-& "$CurrentDir\msixmgr\x64\msixmgr.exe" -Unpack -packagePath $MSIXFilePath.FullName -destination "$($partition.DriveLetter):\notepadplusplus" -applyacls
+& "$CurrentDir\msixmgr\x64\msixmgr.exe" -Unpack -packagePath $LatestNotepadMSIXFilePath.FullName -destination "$($partition.DriveLetter):\$VHDXFileName" -applyacls -fileType VHDX
 
 Dismount-VHD -Path $MSIXVHD
