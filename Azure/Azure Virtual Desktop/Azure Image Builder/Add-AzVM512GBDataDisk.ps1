e3a06fa674c49f3b713aeff2ccbea18b34f3b37f
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-11-04 22:18:11 +0100

    Update

--- Azure/Azure Virtual Desktop/Azure Image Builder/Add-AzVM512GBDataDisk.ps1
+++ Azure/Azure Virtual Desktop/Azure Image Builder/Add-AzVM512GBDataDisk.ps1
@@ -21,21 +21,45 @@ function Get-AzVMCompute {
         return $null
     }
 }
+
+function Get-AzAccessToken {
+    [CmdletBinding(PositionalBinding = $false)]
+    Param(
+    )
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Entering function '$($MyInvocation.MyCommand)'"
+    #Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+
+    $uri = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=https://management.azure.com/"
+
+    try {
+        $AccessToken = Invoke-RestMethod -Uri $uri -Headers @{"Metadata" = "true" } -Method GET -TimeoutSec 5
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Azure Access Token:`r`n$($AccessToken | Out-String)"
+        return $AccessToken
+    }
+    catch {
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+        return $null
+    }
+}
 #endregion
 
 #region Powershell Pre-requisites
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Get-PackageProvider -Name Nuget -ForceBootstrap -Force
 Install-Module Az.Accounts, Az.Compute -Force -Verbose -AllowClobber
 #endregion
 
 #region Azure connection
 #Works because the VM has only one User Managed Identity (else you have to specify -Body @{client_id=$identityClientId} in the Invoke-RestMethod call)
-$Token = Invoke-RestMethod -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=https://management.azure.com/" -Method GET -Headers @{Metadata="true"} -TimeoutSec 5
-Connect-AzAccount -AccessToken $Token.access_token -AccountId $Token.client_id
+#$AccessToken = Invoke-RestMethod -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=https://management.azure.com/" -Method GET -Headers @{Metadata="true"} -TimeoutSec 5
+$AccessToken = Get-AzAccessToken -Verbose
+Connect-AzAccount -AccessToken $AccessToken.access_token -AccountId $AccessToken.client_id
 #endregion
 
 #region Variable definitions
-$ThisVM = Get-AzVMCompute | Get-AzVM
+$ThisVM = Get-AzVMCompute -Verbose | Get-AzVM
 $Location = $ThisVM.Location
 $DataDiskName = "{0}-DataDisk01" -f $ThisVM.Name
 $DataDiskSizeGB = 512
