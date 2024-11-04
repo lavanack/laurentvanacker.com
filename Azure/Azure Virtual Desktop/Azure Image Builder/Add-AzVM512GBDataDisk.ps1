f36c88b3b3c4f47e3bdfb7d8921e2ca2b709928d
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-11-04 13:54:45 +0100

    Initial commit

---
+++ Azure/Azure Virtual Desktop/Azure Image Builder/Add-AzVM512GBDataDisk.ps1
@@ -0,0 +1,50 @@
+﻿#region Function definitions
+#Get The Azure VM Compute Object for the VM executing this function
+function Get-AzVMCompute {
+    [CmdletBinding(PositionalBinding = $false)]
+    Param(
+    )
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Entering function '$($MyInvocation.MyCommand)'"
+    #Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+
+    $uri = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+
+    try {
+        $response = Invoke-RestMethod -Uri $uri -Headers @{"Metadata" = "true" } -Method GET -TimeoutSec 5
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] VM Compute Object:`r`n$($response.compute | Out-String)"
+        return $response.compute
+    }
+    catch {
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+        return $null
+    }
+}
+#endregion
+
+<#
+#region Powershell Pre-requisites
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module Az.Accounts, Az.Compute -Force -Verbose -AllowClobber
+#endregion
+#>
+
+
+#region Variable definitions
+$ThisVM = Get-AzVMCompute | Get-AzVM
+$Location = $ThisVM.Location
+$DataDiskName = "{0}-DataDisk01" -f $ThisVM.Name
+$DataDiskSizeGB = 512
+#$OSDiskType = "Premium_LRS"
+$OSDiskType = $ThisVM.StorageProfile.OsDisk.ManagedDisk.StorageAccountType 
+$ResourceGroupName = $ThisVM.ResourceGroupName
+#endregion
+
+#region Adding Data Disk
+$ThisVMDataDisk01Config = New-AzDiskConfig -SkuName $OSDiskType -Location $Location -CreateOption Empty -DiskSizeGB $DataDiskSizeGB
+$ThisVMDataDisk01 = New-AzDisk -DiskName $DataDiskName -Disk $ThisVMDataDisk01Config -ResourceGroupName $ResourceGroupName
+$ThisVM = Add-AzVMDataDisk -VM $ThisVM -Name $DataDiskName -Caching 'ReadWrite' -CreateOption Attach -ManagedDiskId $ThisVMDataDisk01.Id -Lun 0
+$ThisVM | Update-AzVM
+$Disk = Get-Disk -Number 1 | Where-Object PartitionStyle -EQ "RAW" | Initialize-Disk -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -NewFileSystemLabel Data
+#endregion
