942db8486acada2827443a03b30df7337991499f
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-11-05 13:13:19 +0100

    Update

--- Azure/Azure Virtual Desktop/Azure Image Builder/Add-AzVM512GBDataDisk.ps1
+++ Azure/Azure Virtual Desktop/Azure Image Builder/Add-AzVM512GBDataDisk.ps1
@@ -22,9 +22,11 @@ function Get-AzVMCompute {
     }
 }
 
-function Get-AzAccessToken {
+function Get-MyAzAccessToken {
     [CmdletBinding(PositionalBinding = $false)]
     Param(
+        [Parameter(Mandatory = $false)]
+        [string] $identityClientId
     )
 
     Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Entering function '$($MyInvocation.MyCommand)'"
@@ -33,10 +35,50 @@ function Get-AzAccessToken {
     $uri = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=https://management.azure.com/"
 
     try {
-        $AccessToken = Invoke-RestMethod -Uri $uri -Headers @{"Metadata" = "true" } -Method GET -TimeoutSec 5
+        if ([string]::IsNullOrEmpty($identityClientId)) {
+            #Works because the VM has only one User Managed Identity (else you have to specify -Body @{client_id=$identityClientId} in the Invoke-RestMethod call)
+            $MyAzAccessToken = Invoke-RestMethod -Uri $uri -Headers @{"Metadata" = "true" } -Method GET -TimeoutSec 5
+        } 
+        else {
+            $MyAzAccessToken = Invoke-RestMethod -Uri $uri -Headers @{"Metadata" = "true" } -Body @{client_id = $identityClientId } -Method GET -TimeoutSec 5
+        }
         Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
-        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Azure Access Token:`r`n$($AccessToken | Out-String)"
-        return $AccessToken
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Azure Access Token:`r`n$($MyAzAccessToken | Out-String)"
+        return $MyAzAccessToken
+    }
+    catch {
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+        return $null
+    }
+}
+
+function Get-MyAzResourceGroup {
+    [CmdletBinding(PositionalBinding = $false)]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $SubscriptionId,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ResourceGroupName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $AccessToken
+    )
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Entering function '$($MyInvocation.MyCommand)'"
+    #Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+
+    $uri = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}?api-version=2016-06-01" -f $SubscriptionId, $resourceGroupName
+
+    try {
+        $MyAzResourceGroup = Invoke-WebRequest -Uri $uri -Method GET -Headers @{ Authorization = "Bearer $AccessToken" }
+        #From https://pachehra.blogspot.com/2019/08/managed-identities-azure-ad.html
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] ResourceGroup:`r`n$($MyAzResourceGroup | Out-String)"
+        return $MyAzResourceGroup
     }
     catch {
         Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
@@ -48,18 +90,23 @@ function Get-AzAccessToken {
 #region Powershell Pre-requisites
 #Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Get-PackageProvider -Name Nuget -ForceBootstrap -Force
-Install-Module Az.Accounts, Az.Compute -Force -Verbose -AllowClobber
+#Install-Module Az.Accounts, Az.Compute -Force -Verbose -AllowClobber
+"Az.Accounts", "Az.Compute" |  Where-Object -FilterScript { $_ -notin $(Get-Module -ListAvailable).Name } | ForEach-Object -Process { Install-Module -Name $_ -Force -Verbose -AllowClobber }
 #endregion
 
 #region Azure connection
-#Works because the VM has only one User Managed Identity (else you have to specify -Body @{client_id=$identityClientId} in the Invoke-RestMethod call)
-#$AccessToken = Invoke-RestMethod -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=https://management.azure.com/" -Method GET -Headers @{Metadata="true"} -TimeoutSec 5
-$AccessToken = Get-AzAccessToken -Verbose
-Connect-AzAccount -AccessToken $AccessToken.access_token -AccountId $AccessToken.client_id
+$ThisVMCompute = Get-AzVMCompute -Verbose
+$MyAzAccessToken = Get-MyAzAccessToken -Verbose
+$MyAzResourceGroup = Get-MyAzResourceGroup -SubscriptionId $ThisVMCompute.subscriptionId -ResourceGroupName $ThisVMCompute.resourceGroupName -AccessToken $MyAzAccessToken.access_token -Verbose
+
+$null = Connect-AzAccount -AccessToken $MyAzAccessToken.access_token -AccountId $MyAzAccessToken.client_id
+$null = Disable-AzContextAutosave -Scope Process # Ensures you do not inherit an AzContext
+$AzureContext = $Connection.context  # Connect to Azure with user-assigned managed identity
+$connectionResult = Set-AzContext -SubscriptionId $ThisVMCompute.subscriptionId -DefaultProfile $AzureContext
 #endregion
 
 #region Variable definitions
-$ThisVM = Get-AzVMCompute -Verbose | Get-AzVM
+$ThisVM = $ThisVMCompute | Get-AzVM
 $Location = $ThisVM.Location
 $DataDiskName = "{0}-DataDisk01" -f $ThisVM.Name
 $DataDiskSizeGB = 512
