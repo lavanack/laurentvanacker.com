1b30d582548e9b549bba08ed61044bde15f8852e
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-12-07 13:08:06 +0100

    Adding Azure connection

--- Azure/Azure Automation Account/RequestRunningAzureVirtualMachineJITAccessRunBook.ps1
+++ Azure/Azure Automation Account/RequestRunningAzureVirtualMachineJITAccessRunBook.ps1
@@ -1,12 +1,23 @@
 ﻿#requires -Version 3.0 -Modules Az.Accounts, Az.Resources
-#Modified version from https://luke.geek.nz/azure/turn-on-a-azure-virtual-machine-using-azure-automation/
 
 Param(
 )
 
 $IP  = Get-AutomationVariable -Name IP
 
-#region Varibale definition(s)
+
+#region Azure connection
+# Ensures you do not inherit an AzContext in your dirbook
+Disable-AzContextAutosave -Scope Process
+# Connect to Azure with system-assigned managed identity (Azure Automation account, which has been given VM Start permissions)
+$AzureContext = (Connect-AzAccount -Identity).context
+Write-Output -InputObject $AzureContext
+# set and store context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+Write-Output -InputObject $AzureContext
+#endregion
+
+#region Variable definition(s)
 $RDPPort = 3389
 $JitPolicyTimeInHours = 3
 $JitPolicyName = "Default"
@@ -15,7 +26,7 @@ $VM = $(Get-AzVM -Status | Where-Object -FilterScript { $_.PowerState -match "ru
 #endregion
 
 $AzJitNetworkAccessPolicy = foreach ($CurrentVMJitNetworkAccessPolicyVM in $VM) {
-    Write-Verbose -Message "VM : $($CurrentVMJitNetworkAccessPolicyVM.Name)"
+    Write-Output -InputObject "VM : $($CurrentVMJitNetworkAccessPolicyVM.Name)"
     if ($CurrentVMJitNetworkAccessPolicyVM.Id -in $JitNetworkAccessPolicyVM) {
         $JitPolicy = (@{
                 id    = $CurrentVMJitNetworkAccessPolicyVM.Id
@@ -26,12 +37,12 @@ $AzJitNetworkAccessPolicy = foreach ($CurrentVMJitNetworkAccessPolicyVM in $VM) 
                     })
             })
         $ActivationVM = @($JitPolicy)
-        Write-Host -Object "Requesting Temporary Acces via Just in Time for $($CurrentVMJitNetworkAccessPolicyVM.Name) on port number $RDPPort for maximum $JitPolicyTimeInHours hours from $IP ..."
+        Write-Output -InputObject "Requesting Temporary Acces via Just in Time for $($CurrentVMJitNetworkAccessPolicyVM.Name) on port number $RDPPort for maximum $JitPolicyTimeInHours hours from $IP ..."
         #Get-AzJitNetworkAccessPolicy -ResourceGroupName $($CurrentVMJitNetworkAccessPolicyVM.ResourceGroupName) -Location $CurrentVMJitNetworkAccessPolicyVM.Location -Name $JitPolicyName 
         #Start-AzJitNetworkAccessPolicy -ResourceGroupName $($CurrentVMJitNetworkAccessPolicyVM.ResourceGroupName) -Location $CurrentVMJitNetworkAccessPolicyVM.Location -Name $JitPolicyName -VirtualMachine $ActivationVM | Select-Object -Property *, @{Name = 'endTimeUtc'; Expression = { $JitPolicy.ports.endTimeUtc } }, @{Name = 'startTime'; Expression = { $_.startTimeUtc.ToLocalTime() } }, @{Name = 'endTime'; Expression = { $JitPolicy.ports.endTimeUtc.ToLocalTime() } }
         Start-AzJitNetworkAccessPolicy -ResourceGroupName $($CurrentVMJitNetworkAccessPolicyVM.ResourceGroupName) -Location $CurrentVMJitNetworkAccessPolicyVM.Location -Name $JitPolicyName -VirtualMachine $ActivationVM | Select-Object -Property *, @{Name = 'startTime'; Expression = { $_.startTimeUtc.ToLocalTime() } }, @{Name = 'endTime'; Expression = { $JitPolicy.ports.endTimeUtc.ToLocalTime() } } -ExcludeProperty StartTimeUtc
     }
     else {
-        Write-Warning -Message "Just in Time for is not enabled for $($CurrentVMJitNetworkAccessPolicyVM.Name)"
+        Write-Output -InputObject "[WARNING] Just in Time for is not enabled for $($CurrentVMJitNetworkAccessPolicyVM.Name)"
     }
 }
