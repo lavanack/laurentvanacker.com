b40d34024d58873060e3cdd323adf2059d44f8c2
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-01-20 16:35:43 +0100

    Initial commit

---
+++ Azure/Network Security Group/Test-NSG.ps1
@@ -0,0 +1,132 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5 -Modules Az.Compute, Az.Network, Az.Storage, Az.Resources
+
+[CmdletBinding()]
+param
+(
+)
+
+
+#region function definitions 
+#endregion
+
+Clear-Host
+$Error.Clear()
+
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir 
+
+#region Defining variables 
+#region Building an Hashtable to get the shortname of every Azure location based on a JSON file on the Github repository of the Azure Naming Tool
+$AzLocation = Get-AzLocation | Select-Object -Property Location, DisplayName | Group-Object -Property DisplayName -AsHashTable -AsString
+$ANTResourceLocation = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/main/src/repository/resourcelocations.json
+$shortNameHT = $ANTResourceLocation | Select-Object -Property name, shortName, @{Name = 'Location'; Expression = { $AzLocation[$_.name].Location } } | Where-Object -FilterScript { $_.Location } | Group-Object -Property Location -AsHashTable -AsString
+#endregion
+
+# Login to your Azure subscription.
+While (-not(Get-AzContext)) {
+    Connect-AzAccount
+    #Get-AzSubscription | Out-GridView -OutputMode Single -Title "Select your Azure Subscription" | Select-AzSubscription
+    #$Subscription = Get-AzSubscription -SubscriptionName $SubscriptionName -ErrorAction Ignore
+    #Select-AzSubscription -SubscriptionName $SubscriptionName | Select-Object -Property *
+}
+
+$AzureVMNameMaxLength = 15
+$RDPPort = 3389
+$Location = "eastus2"
+#$VMSize = "Standard_D4s_v5"
+$LocationShortName = $shortNameHT[$Location].shortName
+#Naming convention based on https://github.com/microsoft/CloudAdoptionFramework/tree/master/ready/AzNamingTool
+$ResourceGroupPrefix = "rg"
+$NetworkSecurityGroupPrefix = "nsg"
+$VirtualNetworkPrefix = "vnet"
+$SubnetPrefix = "snet"
+$NICPrefix = "nic"
+$Project = "nsg"
+$Role = "poc"
+#$DigitNumber = 4
+$DigitNumber = $AzureVMNameMaxLength - ($VirtualMachinePrefix + $Project + $Role + $LocationShortName).Length
+$Instance = Get-Random -Minimum 0 -Maximum $([long]([Math]::Pow(10, $DigitNumber)))
+
+$NetworkSecurityGroupName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $NetworkSecurityGroupPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$VirtualNetworkName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $VirtualNetworkPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$SubnetName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $SubnetPrefix, $Project, $Role, $LocationShortName, $Instance                       
+$ResourceGroupName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $ResourceGroupPrefix, $Project, $Role, $LocationShortName, $Instance                       
+
+$NetworkSecurityGroupName = $NetworkSecurityGroupName.ToLower()
+$VirtualNetworkName = $VirtualNetworkName.ToLower()
+$SubnetName = $SubnetName.ToLower()
+$ResourceGroupName = $ResourceGroupName.ToLower()
+$VirtualNetworkAddressSpace = "10.10.0.0/16" # Format 10.10.0.0/16
+$SubnetIPRange = "10.10.0.0/19" # Format 10.10.1.0/24                         
+$MyPublicIp = (Invoke-WebRequest -uri "https://ipv4.seeip.org").Content
+
+
+$ResourceGroup = Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction Ignore 
+if ($ResourceGroup) {
+    #Step 0: Remove previously existing Azure Resource Group with the same name
+    $ResourceGroup | Remove-AzResourceGroup -Force -Verbose
+}
+
+#region Define Variables needed for Virtual Machine
+Write-Verbose "`$NetworkSecurityGroupName: $NetworkSecurityGroupName"         
+Write-Verbose "`$VirtualNetworkName: $VirtualNetworkName"         
+Write-Verbose "`$SubnetName: $SubnetName"       
+Write-Verbose "`$ResourceGroupName: $ResourceGroupName"
+#endregion
+#endregion
+
+#Create Azure Resource Group
+# Create Resource Groups
+$ResourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force
+
+
+$SecurityRules = @(
+    #region Inbound
+    #RDP only for my public IP address
+    New-AzNetworkSecurityRuleConfig -Name RDPRule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 300 -SourceAddressPrefix $MyPublicIp -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange $RDPPort
+    #HTTP only for my public IP address
+    New-AzNetworkSecurityRuleConfig -Name HTTPRule -Description "Allow HTTP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 301 -SourceAddressPrefix $MyPublicIp -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+    #HTTPS only for my public IP address
+    New-AzNetworkSecurityRuleConfig -Name HTTPSRule -Description "Allow HTTPS" -Access Allow -Protocol Tcp -Direction Inbound -Priority 302 -SourceAddressPrefix $MyPublicIp -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 443
+    #endregion
+)
+
+$NetworkSecurityGroup = New-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Location $Location -Name $NetworkSecurityGroupName -SecurityRules $SecurityRules -Force
+
+#Create Azure Virtual network using the virtual network subnet configuration
+$VirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $VirtualNetworkName  -AddressPrefix $VirtualNetworkAddressSpace -Location $Location
+Add-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $VirtualNetwork -AddressPrefix $SubnetIPRange -NetworkSecurityGroupId $NetworkSecurityGroup.Id
+
+$VirtualNetwork = Set-AzVirtualNetwork -VirtualNetwork $VirtualNetwork
+$Subnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $VirtualNetwork
+
+$NicNumber = 6356
+$Jobs = 1..$NicNumber | ForEach-Object -Process {
+    $NICName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}-{5:D$DigitNumber}" -f $NICPrefix, $Project, $Role, $LocationShortName, $Instance, $_
+    Write-Host -Object "Creating NIC '$NICName' ..."
+    $PercentComplete = ($_/$NicNumber) 
+    Write-Progress -Activity "[$_/$NicNumber] Creating NIC '$NICName'" -Status ('Processing {0:p0}' -f $PercentComplete) -PercentComplete ($PercentComplete*100)
+    #Create Network Interface Card 
+    New-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Subnet.Id -NetworkSecurityGroupId $NetworkSecurityGroup.Id -AsJob
+}
+#$Jobs | Receive-Job -Wait -AutoRemoveJob
+Write-Progress -Completed -Activity 'All NICs have been created'
