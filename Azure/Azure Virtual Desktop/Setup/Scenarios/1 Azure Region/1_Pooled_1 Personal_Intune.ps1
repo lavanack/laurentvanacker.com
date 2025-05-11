639a18fe895627090e4a6c26dfddad0de5153c16
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-05-11 11:26:09 +0200

    Update

---
+++ Azure/Azure Virtual Desktop/Setup/Scenarios/1 Azure Region/1_Pooled_1 Personal_Intune.ps1
@@ -0,0 +1,5 @@
+﻿#Deploy 1 Personal HostPool with Intune
+[PersonalHostPool]::new($HostPoolSessionCredentialKeyVault, $PrimaryRegionSubnet.Id).SetVMNumberOfInstances(1).EnableIntune().EnableSpotInstance()
+
+#Deploy 1 Pooled HostPool with Intune
+[PooledHostPool]::new($HostPoolSessionCredentialKeyVault, $PrimaryRegionSubnet.Id).SetVMNumberOfInstances(1).EnableIntune().EnableSpotInstance()
