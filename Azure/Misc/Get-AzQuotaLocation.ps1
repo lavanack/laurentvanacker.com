ff9fe39e222ed603525d3cd429953eec2f517c83
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-05-26 14:55:37 +0200

    Initial commit

---
+++ Azure/Misc/Get-AzQuotaLocation.ps1
@@ -0,0 +1,67 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5 -Modules Az.Accounts, Az.Compute, Az.Quota, Az.Resources
+
+[CmdletBinding(PositionalBinding = $false)]
+Param (
+)
+
+
+#region Function Definitions
+function Get-AzQuotaLocation {
+    [CmdletBinding(PositionalBinding = $false)]
+    Param (
+        [ValidateScript({$_ -in $((Get-AzLocation).Location)})]
+        [string[]] $Location = $((Get-AzLocation).Location),
+        #[ValidateScript({$_ -in $((Get-AzComputeResourceSku | Where-Object { $_.ResourceType -eq "virtualMachines" }).Family)})]
+        [string[]] $ResourceName=@("virtualMachines")
+    )
+
+    $QuotaPerAzLocation = foreach ($CurrentLocation in $Location) {
+        Write-Verbose -Message "Processing '$CurrentLocation' Azure Location"
+        foreach ($currentResourceName in $ResourceName) {
+            Write-Verbose -Message "Processing '$currentResourceName' Azure Resource"
+            $Limit = (Get-AzQuota -Scope "/subscriptions/$((Get-AzContext).Subscription.Id)/providers/Microsoft.Compute/locations/$CurrentLocation" -ResourceName $currentResourceName -ErrorAction Ignore).Limit.Value
+            if ([string]::IsNullOrEmpty($Limit)) {
+                $Limit = "N/A"
+                Write-Warning "No data for '$CurrentLocation'"
+            }
+            [pscustomobject]@{"Location"=$CurrentLocation; ResourceName = $currentResourceName; Limit = $Limit} 
+        }
+    }
+    $QuotaPerAzLocation
+}
+#endregion
+
+
+Clear-Host
+$Error.Clear()
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir
+
+#region Login to your Azure subscription.
+While (-not(Get-AzAccessToken -ErrorAction Ignore)) {
+    Connect-AzAccount
+}
+#endregion
+
+Get-AzQuotaLocation -Location francecentral -ResourceName StandardDASv5Family -Verbose
+#Get-AzQuotaLocation
+#Get-AzQuotaLocation -Location francecentral, eastus2 -ResourceName StandardDASv5Family, StandardDv5Family -Verbose
