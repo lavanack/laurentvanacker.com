8bc0a0d811b3e36250b5b884bacacae0a1fcf3fa
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-02 16:18:28 +0200

    Update

--- Azure/Networking/VPN Gateway/New-AzP2SVPN.ps1
+++ Azure/Networking/VPN Gateway/New-AzP2SVPN.ps1
@@ -26,7 +26,6 @@ function New-AzP2SVPN {
     (
         [ValidateScript({ $_ -in (Get-AzLocation).Location })]
         [string] $Location = "eastus2",
-
         [switch] $Connect
     )
 
@@ -75,10 +74,10 @@ function New-AzP2SVPN {
     $VPNClientAddressPool = "172.16.201.0/24"
     #$SubnetConfigFrontendName = "Frontend"
     $SubnetConfigFrontendName = $SubnetName
-    $SubnetConfigGWName = "GatewaySubnet"
+    $SubnetConfigGatewayName = "GatewaySubnet"
     $vnetAddressPrefix = "10.1.0.0/16"
     $SubnetConfigFrontendAddressPrefix = "10.1.0.0/24"
-    $SubnetConfigGWAddressPrefix = "10.1.255.0/27"
+    $SubnetConfigGatewayAddressPrefix = "10.1.255.0/27"
     $ClearTextPassword = 'P@ssw0rd'
     $SecurePassword = ConvertTo-SecureString -String $ClearTextPassword -AsPlainText -Force
     #endregion
@@ -90,14 +89,14 @@ function New-AzP2SVPN {
     $ResourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
     $VirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Location $Location -Name $VirtualNetworkName -AddressPrefix $vnetAddressPrefix
     $SubnetConfigFrontend = Add-AzVirtualNetworkSubnetConfig -Name $SubnetConfigFrontendName -AddressPrefix $SubnetConfigFrontendAddressPrefix -VirtualNetwork $VirtualNetwork
-    $SubnetConfigGW = Add-AzVirtualNetworkSubnetConfig -Name $SubnetConfigGWName -AddressPrefix $SubnetConfigGWAddressPrefix -VirtualNetwork $VirtualNetwork
+    $SubnetConfigGateway = Add-AzVirtualNetworkSubnetConfig -Name $SubnetConfigGatewayName -AddressPrefix $SubnetConfigGatewayAddressPrefix -VirtualNetwork $VirtualNetwork
     $VirtualNetwork | Set-AzVirtualNetwork
     #endregion
 
     #region Create the VPN gateway
     $GatewayPIP = New-AzPublicIpAddress -Name $PublicIpAddressName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Static -Sku Standard
     $VirtualNetwork = Get-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName
-    $GatewaySubnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetConfigGWName -VirtualNetwork $VirtualNetwork
+    $GatewaySubnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetConfigGatewayName -VirtualNetwork $VirtualNetwork
     $GatewayIPConfig = New-AzVirtualNetworkGatewayIpConfig -Name $GatewayIpConfigName -SubnetId $GatewaySubnet.Id -PublicIpAddressId $GatewayPIP.Id
     #$Gateway = New-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location $Location -IpConfigurations $GatewayIPConfig -GatewayType Vpn -VpnType RouteBased -EnableBgp $false -GatewaySku VpnGw2 -VpnGatewayGeneration "Generation2" -VpnClientProtocol IkeV2,OpenVPN
     $Gateway = New-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location $Location -IpConfigurations $GatewayIPConfig -GatewayType Vpn -VpnType RouteBased -EnableBgp $false -GatewaySku VpnGw1 -VpnGatewayGeneration "Generation1" -VpnClientProtocol IkeV2,OpenVPN
@@ -197,6 +196,187 @@ function New-AzP2SVPN {
     #endregion
 }
 
+function Add-AzP2SVPN {
+    [CmdletBinding(PositionalBinding = $false)]
+    param
+    (
+		[Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $False)]
+        [Microsoft.Azure.Commands.Network.Models.PSVirtualNetwork] $VirtualNetwork,
+        [switch] $Connect
+    )
+
+
+    #region Defining variables 
+    #region Building an Hashtable to get the shortname of every Azure location based on a JSON file on the Github repository of the Azure Naming Tool
+    $AzLocation = Get-AzLocation | Select-Object -Property Location, DisplayName | Group-Object -Property DisplayName -AsHashTable -AsString
+    $ANTResourceLocation = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/main/src/repository/resourcelocations.json
+    $ResourceLocationShortNameHT = $ANTResourceLocation | Select-Object -Property name, shortName, @{Name = 'Location'; Expression = { $AzLocation[$_.name].Location } } | Where-Object -FilterScript { $_.Location } | Group-Object -Property Location -AsHashTable -AsString
+    #endregion
+
+    #region Building an Hashtable to get the shortname of every Azure resource based on a JSON file on the Github repository of the Azure Naming Tool
+    $Result = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/refs/heads/main/src/repository/resourcetypes.json 
+    $ResourceTypeShortNameHT = $Result | Where-Object -FilterScript { $_.property -in @('', 'Windows')} | Select-Object -Property resource, shortName, lengthMax | Group-Object -Property resource -AsHashTable -AsString
+    #endregion
+
+    #region Variables
+    $TimeStamp = "{0:yyyyMMddHHmmss}" -f (Get-Date)
+
+    $LocationShortName = $ResourceLocationShortNameHT[$Location].shortName
+    #Naming convention based on https://github.com/mspnp/AzureNamingTool/blob/main/src/repository/resourcetypes.json
+    $AzureVMNameMaxLength = $ResourceTypeShortNameHT["Compute/virtualMachines"].lengthMax
+    $VirtualMachinePrefix = $ResourceTypeShortNameHT["Compute/virtualMachines"].ShortName
+    $VirtualNetworkPrefix = $ResourceTypeShortNameHT["Network/virtualNetworks"].ShortName
+    $SubnetPrefix = $ResourceTypeShortNameHT["Network/virtualnetworks/subnets"].ShortName
+    $VirtualNetworkGatewayPrefix = $ResourceTypeShortNameHT["Network/virtualNetworkGateways"].ShortName
+    $ResourceGroupPrefix = $ResourceTypeShortNameHT["Resources/resourcegroups"].ShortName
+    $publicIPAddressPrefix = $ResourceTypeShortNameHT["Network/publicIPAddresses"].ShortName
+
+    $ResourceGroupName = $VirtualNetwork.ResourceGroupName
+    $VirtualNetworkName = $VirtualNetwork.Name
+    $Location = $VirtualNetwork.Location
+
+    if ($VirtualNetworkName -match $("{0}-(?<Project>\w+)-(?<Role>\w+)-(?<LocationShortName>\w+)-(?<Instance>\d+)" -f $VirtualNetworkPrefix)) {
+        $Project = $Matches["Project"]
+        $Role = $Matches["Role"]
+        #$LocationShortName = $Matches["LocationShortName"]
+        $Instance = $Matches["Instance"]
+        $DigitNumber = $Instance.Length
+        $SubnetConfigGatewayAddressPrefix = $VirtualNetwork.AddressSpace.AddressPrefixes -replace "(\d+)\.(\d+)\.(\d+)\.(\d+)/(\d+)", '$1.$2.255.0/27'
+    } 
+    else {
+        $Project = "p2s"
+        $Role = "vpn"
+        #$DigitNumber = 4
+        $DigitNumber = $AzureVMNameMaxLength - ($VirtualMachinePrefix + $Project + $Role + $LocationShortName).Length
+        $Instance = Get-Random -Minimum 0 -Maximum $([long]([Math]::Pow(10, $DigitNumber)))
+    }
+
+    $VirtualNetworkGatewayName = "{0}-{1}-{2}-{3}-{4:D$DigitNumber}" -f $VirtualNetworkGatewayPrefix, $Project, $Role, $LocationShortName, $Instance                       
+    $GatewayIpConfigName = "{0}-ipconfig-{1:D$DigitNumber}" -f $VirtualNetworkGatewayPrefix, $Instance         
+    $PublicIpAddressName = "{0}-{1}" -f $publicIPAddressPrefix, $GatewayIpConfigName
+    $VirtualNetworkGatewayName = $VirtualNetworkGatewayName.ToLower()
+    $GatewayIpConfigName = $GatewayIpConfigName.ToLower()
+    $PublicIpAddressName = $PublicIpAddressName.ToLower()
+
+    $VPNClientAddressPool = "172.16.201.0/24"
+    #$SubnetConfigFrontendName = "Frontend"
+    #We take the first subnet as Frontend subnet
+    $SubnetConfigFrontendName = ($VirtualNetwork.Subnets | Select-Object -First 1).Name
+    $SubnetConfigGatewayName = "GatewaySubnet"
+    $SubnetConfigGatewayAddressPrefix = $VirtualNetwork.AddressSpace.AddressPrefixes -replace "(\d+)\.(\d+)\.(\d+)\.(\d+)/(\d+)", '$1.$2.255.0/27'
+    $ClearTextPassword = 'P@ssw0rd'
+    $SecurePassword = ConvertTo-SecureString -String $ClearTextPassword -AsPlainText -Force
+    #endregion
+
+    $DestinationFolder = New-Item -Path $(Join-Path -Path $CurrentDir -ChildPath $TimeStamp) -ItemType Directory -Force
+
+    #region Create the Subnet
+    $SubnetConfigGateway = Add-AzVirtualNetworkSubnetConfig -Name $SubnetConfigGatewayName -AddressPrefix $SubnetConfigGatewayAddressPrefix -VirtualNetwork $VirtualNetwork
+    $VirtualNetwork | Set-AzVirtualNetwork
+    #endregion
+
+    #region Create the VPN gateway
+    $GatewayPIP = New-AzPublicIpAddress -Name $PublicIpAddressName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Static -Sku Standard
+    $VirtualNetwork = Get-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName
+    $GatewaySubnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetConfigGatewayName -VirtualNetwork $VirtualNetwork
+    $GatewayIPConfig = New-AzVirtualNetworkGatewayIpConfig -Name $GatewayIpConfigName -SubnetId $GatewaySubnet.Id -PublicIpAddressId $GatewayPIP.Id
+    #$Gateway = New-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location $Location -IpConfigurations $GatewayIPConfig -GatewayType Vpn -VpnType RouteBased -EnableBgp $false -GatewaySku VpnGw2 -VpnGatewayGeneration "Generation2" -VpnClientProtocol IkeV2,OpenVPN
+    $Gateway = New-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location $Location -IpConfigurations $GatewayIPConfig -GatewayType Vpn -VpnType RouteBased -EnableBgp $false -GatewaySku VpnGw1 -VpnGatewayGeneration "Generation1" -VpnClientProtocol IkeV2,OpenVPN
+    #endregion
+
+    #region Add the VPN client address pool
+    $Gateway = Get-AzVirtualNetworkGateway -ResourceGroupName $ResourceGroupName -Name $VirtualNetworkGatewayName
+    Set-AzVirtualNetworkGateway -VirtualNetworkGateway $Gateway -VpnClientAddressPool $VPNClientAddressPool
+    #endregion
+
+    #region Generate certificates
+    #region Root certificate
+    $params = @{
+        Type = 'Custom'
+        Subject = 'CN=P2SRootCert'
+        KeySpec = 'Signature'
+        KeyExportPolicy = 'Exportable'
+        KeyUsage = 'CertSign'
+        KeyUsageProperty = 'Sign'
+        KeyLength = 2048
+        HashAlgorithm = 'sha256'
+        NotAfter = (Get-Date).AddMonths(24)
+        CertStoreLocation = 'Cert:\CurrentUser\My'
+    }
+    $RootCert = New-SelfSignedCertificate @params
+    #region Exporting Certificate
+    $RootCertFilePath = Join-Path -Path $DestinationFolder -ChildPath $("{0}_{1}.cer" -f $($params.Subject -replace "CN="), $TimeStamp)
+    $content = @(
+        '-----BEGIN CERTIFICATE-----'
+        [System.Convert]::ToBase64String($RootCert.RawData, 'InsertLineBreaks')
+        '-----END CERTIFICATE-----'
+    )
+    $content | Out-File -FilePath $RootCertFilePath -Encoding ascii
+    #endregion
+    #region Exporting Certificate with Private Key
+    $RootCertPFXFilePath = Join-Path -Path $DestinationFolder -ChildPath $("{0}_{1}.pfx" -f $($params.Subject -replace "CN="), $TimeStamp)
+    $RootCert | Export-PfxCertificate -FilePath $RootCertPFXFilePath -Password $SecurePassword -Force
+    #endregion
+    #endregion
+
+    #region Client certificate
+    $params = @{
+           Type = 'Custom'
+           Subject = 'CN=P2SChildCert'
+           DnsName = 'P2SChildCert'
+           KeySpec = 'Signature'
+           KeyExportPolicy = 'Exportable'
+           KeyLength = 2048
+           HashAlgorithm = 'sha256'
+           NotAfter = (Get-Date).AddMonths(18)
+           CertStoreLocation = 'Cert:\CurrentUser\My'
+           Signer = $RootCert
+           TextExtension = @('2.5.29.37={text}1.3.6.1.5.5.7.3.2')
+    }
+    $ClientCert = New-SelfSignedCertificate @params
+    #region Exporting Certificate with Private Key
+    $ClientCertPFXFilePath = Join-Path -Path $DestinationFolder -ChildPath $("{0}_{1}.pfx" -f $($params.Subject -replace "CN="), $TimeStamp)
+    $ClientCert | Export-PfxCertificate -FilePath $ClientCertPFXFilePath -Password $SecurePassword -Force
+    #endregion
+
+    #endregion
+    #endregion
+
+    #region Upload root certificate public key information
+    $cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2($RootCertFilePath)
+    $CertBase64 = [system.convert]::ToBase64String($cert.RawData)
+
+    $P2SRootCertName = Split-Path -Path $RootCertFilePath -Leaf
+    Add-AzVpnClientRootCertificate -VpnClientRootCertificateName $P2SRootCertName -VirtualNetworkGatewayname $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -PublicCertData $CertBase64
+    #endregion
+
+    #region Generate and download the VPN client profile configuration package
+    $profile = New-AzVpnClientConfiguration -ResourceGroupName $ResourceGroupName -Name $VirtualNetworkGatewayName -AuthenticationMethod "EapTls"
+    #Removing the query string
+	$DestinationFile = Join-Path -Path $DestinationFolder -ChildPath $((Split-Path $profile.VPNProfileSASUrl -Leaf) -replace "\?.*$")
+    #Adding a timestamp to the filename
+    $DestinationFile = $DestinationFile -replace "(\.\w*)$", $('_{0}$1' -f $TimeStamp)
+    #Creating a dedicater folder with the same name that the filename above but without the extension (for extracting the files from the downloaded zip file)
+    $DestinationPath = $DestinationFile -replace "(\.\w*)$", $('_{0}' -f $TimeStamp)
+
+    Start-BitsTransfer -Source $profile.VPNProfileSASUrl -Destination $DestinationFile
+    Expand-Archive -Path $DestinationFile -DestinationPath $DestinationPath
+    $VpnProfileSetupPowerShellScript = (Get-ChildItem -Path $DestinationPath -Filter VpnProfileSetup.ps1 -Recurse).FullName
+    #Return an error if the VpnConnection doesn't exist (around line 61 - an -ErrorAction Ignore could solve this)
+	& $VpnProfileSetupPowerShellScript -Force 2>&1 | Out-Null
+    #endregion
+
+    if ($Connect) {
+        #region VPN connection
+        rasdial $VirtualNetworkName
+        #endregion
+
+        #region VPN disconnection
+        #rasdial $VirtualNetworkName /DISCONNECT
+        #endregion
+    }
+    #endregion
+}
 
 #region Main Code
 Clear-Host
@@ -208,5 +388,6 @@ $CurrentScript = $MyInvocation.MyCommand.Path
 $CurrentDir = Split-Path -Path $CurrentScript -Parent
 Set-Location -Path $CurrentDir
 
-New-AzP2SVPN -Connect -Verbose
+#New-AzP2SVPN -Connect -Verbose
+Get-AzVirtualNetwork -Name "vnet-p2s-vpn-use2-171" | Add-AzP2SVPN -Connect -Verbose
 #endregion
