f2ce53cfc06184cdab3d530290889e87661d9c0a
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-08 13:42:07 +0200

    Update

--- Azure/Azure Automation Account/Azure Disk/SetSSDDiskRunBook.ps1
+++ Azure/Azure Automation Account/Azure Disk/SetSSDDiskRunBook.ps1
@@ -15,16 +15,17 @@ Write-Output -InputObject $AzureContext
 #endregion
 
 #region Set Disk Configuration
+$OldDiskSku = "Standard_LRS"
+$NewDiskSku = "StandardSSD_LRS"
 $StoppedVM = Get-AzVM -Status | Where-Object -FilterScript { $_.PowerState -match "VM deallocated" }
-$HDDDisks = ($StoppedVM.StorageProfile.OsDisk | Get-AzDisk) | Where-Object -FilterScript { $_.Sku.Name -eq "Standard_LRS"}
+$HDDDisks = ($StoppedVM.StorageProfile.OsDisk | Get-AzDisk) | Where-Object -FilterScript { $_.Sku.Name -eq $OldDiskSku }
 if ($StoppedVM.StorageProfile.DataDisks) {
-    $HDDDisks += ($StoppedVM.StorageProfile.DataDisks | Get-AzDisk) | Where-Object -FilterScript { $_.Sku.Name -eq "Standard_LRS"}
+    $HDDDisks += ($StoppedVM.StorageProfile.DataDisks | Get-AzDisk) | Where-Object -FilterScript { $_.Sku.Name -eq $OldDiskSku }
 }
 
-foreach ($CurrentHDDDisk in $HDDDisks)
-{
+foreach ($CurrentHDDDisk in $HDDDisks) {
     Write-Output -InputObject "`$CurrentHDDDisk : $($CurrentHDDDisk.Name)"
-    $CurrentHDDDisk.Sku = [Microsoft.Azure.Management.Compute.Models.DiskSku]::new("StandardSSD_LRS")
-	$CurrentHDDDisk | Update-AzDisk
+    $CurrentHDDDisk.Sku = [Microsoft.Azure.Management.Compute.Models.DiskSku]::new($NewDiskSku)
+    $CurrentHDDDisk | Update-AzDisk
 }
 #endregion
