f729d98c969c1e1fd530884d4a9b79d970ab4822
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-08 13:36:43 +0200

    Initial commit

---
+++ Azure/Azure Automation Account/Azure Disk/SetSSDDiskRunBook.ps1
@@ -0,0 +1,30 @@
+﻿#requires -Version 3.0 -Modules Az.Accounts, Az.Resources
+
+Param(
+)
+
+#region Azure connection
+# Ensures you do not inherit an AzContext in your dirbook
+Disable-AzContextAutosave -Scope Process
+# Connect to Azure with system-assigned managed identity (Azure Automation account)
+$AzureContext = (Connect-AzAccount -Identity).context
+Write-Output -InputObject $AzureContext
+# set and store context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+Write-Output -InputObject $AzureContext
+#endregion
+
+#region Set Disk Configuration
+$StoppedVM = Get-AzVM -Status | Where-Object -FilterScript { $_.PowerState -match "VM deallocated" }
+$HDDDisks = ($StoppedVM.StorageProfile.OsDisk | Get-AzDisk) | Where-Object -FilterScript { $_.Sku.Name -eq "Standard_LRS"}
+if ($StoppedVM.StorageProfile.DataDisks) {
+    $HDDDisks += ($StoppedVM.StorageProfile.DataDisks | Get-AzDisk) | Where-Object -FilterScript { $_.Sku.Name -eq "Standard_LRS"}
+}
+
+foreach ($CurrentHDDDisk in $HDDDisks)
+{
+    Write-Output -InputObject "`$CurrentHDDDisk : $($CurrentHDDDisk.Name)"
+    $CurrentHDDDisk.Sku = [Microsoft.Azure.Management.Compute.Models.DiskSku]::new("StandardSSD_LRS")
+	$CurrentHDDDisk | Update-AzDisk
+}
+#endregion
