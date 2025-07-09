2f1d78e296de121b9e601110b8fc7479ca514321
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-09 17:22:52 +0200

    Update

--- Azure/Azure Virtual Desktop/Setup/Scenarios/1 Azure Region/1_Pooled_EntraID_AzureAppAttach.ps1
+++ Azure/Azure Virtual Desktop/Setup/Scenarios/1 Azure Region/1_Pooled_EntraID_AzureAppAttach.ps1
@@ -1,2 +1,2 @@
-#Deploy a Pooled HostPool with 3 (default value) Session Hosts (EntraID joined) and with FSLogix Cloud Cache and Azure AppAttach
+#Deploy a Pooled HostPool with 3 (default value) Session Hosts (EntraID joined) and Azure AppAttach
 [PooledHostPool]::new($HostPoolSessionCredentialKeyVault, $PrimaryRegionSubnet.Id).SetIdentityProvider([IdentityProvider]::MicrosoftEntraID)
