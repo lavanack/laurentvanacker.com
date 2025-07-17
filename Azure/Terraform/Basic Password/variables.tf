4739e8439e501d4dd7c3711975f873d417c0e442
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-17 12:40:41 +0200

    Initial Commit

---
+++ Azure/Terraform/Basic Password/variables.tf
@@ -0,0 +1,12 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "lavanack"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "eastus2"
+}
