ba782fc1cb508baba41e30ae070cc7f567d71728
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-21 19:03:56 +0200

    Update

--- Azure/Terraform/Github Actions/main.tf
+++ Azure/Terraform/Github Actions/main.tf
@@ -10,12 +10,14 @@ terraform {
     storage_account_name = "sttfghactuse23726"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "rg-tf-sample-use2-3726" {
