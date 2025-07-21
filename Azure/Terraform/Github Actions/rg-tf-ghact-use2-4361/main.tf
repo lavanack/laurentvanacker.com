60b39be5d05f35dbdc3acc9c9d071436f6d1787b
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-21 15:44:30 +0200

    Initial commit

---
+++ Azure/Terraform/Github Actions/rg-tf-ghact-use2-4361/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "rg-tf-ghact-use2-4361"
+      storage_account_name = "sttfghactuse24361"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-tf-sample-use2-4361" {
+  name     = "rg-tf-sample-use2-4361"
+  location = "eastus2"
+}
+
+
+
+
