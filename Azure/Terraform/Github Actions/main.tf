6d6612e431f409aa69e24558502405ba6bd98a65
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-21 18:22:20 +0200

    Update

--- Azure/Terraform/Github Actions/main.tf
+++ Azure/Terraform/Github Actions/main.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-tf-ghact-use2-4361"
-    storage_account_name = "sttfghactuse24361"
+    resource_group_name  = "rg-tf-ghact-use2-3726"
+    storage_account_name = "sttfghactuse23726"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
@@ -18,7 +18,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-tf-sample-use2-4361" {
-  name     = "rg-tf-sample-use2-4361"
+resource "azurerm_resource_group" "rg-tf-sample-use2-3726" {
+  name     = "rg-tf-sample-use2-3726"
   location = "eastus2"
 }
+
+
+
+
+
