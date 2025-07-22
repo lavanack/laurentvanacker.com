eeb6e183497e33bdd8f7e481356c5e6462e7f330
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-07-22 17:30:47 +0200

    Update

---
+++ Azure/Terraform/Github Actions/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-ghact-use2-3953"
+    storage_account_name = "sttfghactuse23953"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "azurerm_resource_group" "rg-tf-sample-use2-3953" {
+  name     = "rg-tf-sample-use2-3953"
+  location = "eastus2"
+}
+
+
+
+
