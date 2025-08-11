e6fa25eec4ff455d37397f9896561116b43101f9
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-08-11 13:50:55 +0200

    Update for count

---
+++ Azure/Terraform/Misc/count/Bad/bad-count-sample.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_names" {
+  type = list(string)
+  default = [
+    "rg-tf-dev",
+    "rg-tf-test", # What happens if we remove this item from the middle of the list?
+    "rg-tf-prod"
+  ]
+}
+
+variable "region" {
+  type = string
+  default = "eastus2"
+}
+
+resource "azurerm_resource_group" "count_bad" {
+  count    = length(var.resource_group_names)
+  name     = var.resource_group_names[count.index]
+  location = var.region
+}
