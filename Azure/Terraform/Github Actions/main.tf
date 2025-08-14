805c268f228af2f78d90fd682359643fd5e6eb71
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-08-14 11:59:11 +0200

    Testing GitHub Actions

--- Azure/Terraform/Github Actions/main.tf
+++ Azure/Terraform/Github Actions/main.tf
@@ -60,6 +60,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   }
 }
 
+
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
   name                = "${random_pet.prefix.id}-nic"
