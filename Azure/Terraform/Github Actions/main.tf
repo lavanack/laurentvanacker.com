d941920c8436b1b2cd579def11f371972904b103
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-08-14 13:35:44 +0200

    Updating Port

--- Azure/Terraform/Github Actions/main.tf
+++ Azure/Terraform/Github Actions/main.tf
@@ -48,6 +48,8 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     destination_address_prefix = "*"
   }
 
+
+
   security_rule {
     name                       = "web"
     priority                   = 1001
