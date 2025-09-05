b97c94dc89a5d4884aa08971b6ea0fb8d517c8fc
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-09-05 15:30:07 +0200

    Adding Software Setup Management

--- Azure/Desired State Configuration/Azure Machine Configuration/Windows/3 - AzureAutomanageMachineConfigurationWithSASToken.ps1
+++ Azure/Desired State Configuration/Azure Machine Configuration/Windows/3 - AzureAutomanageMachineConfigurationWithSASToken.ps1
@@ -60,6 +60,7 @@ $StorageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName
 $StorageAccountName = $StorageAccount.StorageAccountName
 $StorageGuestConfigurationContainerName = "guestconfiguration"
 $StorageCertificateContainerName = "certificates"
+$StorageSoftwareContainerName = "softwares"
 #Adding a 7-day expiration time from now for the SAS Token
 $StartTime = Get-Date
 $ExpiryTime = $StartTime.AddDays(7)
@@ -122,10 +123,27 @@ $Context = New-AzStorageContext -StorageAccountName $StorageAccountName -UseConn
 $storageAccount | Get-AzStorageContainer | Get-AzStorageBlob | Remove-AzStorageBlob
 #endregion
 
+#region Software Setup Management
+# Creates a new certificate container
+if (-not($storageAccount | Get-AzStorageContainer -Name $StorageSoftwareContainerName -ErrorAction Ignore)) {
+    New-AzStorageContainer -Name $StorageSoftwareContainerName -Context $Context #-Permission Blob
+}
+
+#region Downloading LogParser in the script folder
+$LogParserURI = "https://download.microsoft.com/download/f/f/1/ff1819f9-f702-48a5-bbc7-c9656bc74de8/LogParser.msi"
+$Destination = Join-Path -Path $PSScriptRoot -ChildPath $(Split-Path -Path $LogParserURI -Leaf)
+Start-BitsTransfer -Source $LogParserURI -Destination $Destination
+#endregion
+
+#region Adding LogParser Installer to the container
+(Get-Item -Path $Destination) | Set-AzStorageBlobContent -Container $StorageSoftwareContainerName -Context $Context -Force
+#endregion
+#endregion
+
 #region Self-signed Certificate Management
 # Creates a new certificate container
 if (-not($storageAccount | Get-AzStorageContainer -Name $StorageCertificateContainerName -ErrorAction Ignore)) {
-    New-AzStorageContainer -Name $StorageCertificateContainerName -Context $Context#-Permission Blob
+    New-AzStorageContainer -Name $StorageCertificateContainerName -Context $Context #-Permission Blob
 }
 
 #region Generating Self-signed Certificates, exporting them as .cer files and delete them from certificate store
@@ -170,7 +188,7 @@ foreach ($CurrentDSCConfiguration in $DSCConfigurations) {
 
     # Creates a new guest configuration container
     if (-not($storageAccount | Get-AzStorageContainer -Name $StorageGuestConfigurationContainerName -ErrorAction Ignore)) {
-        New-AzStorageContainer -Name $StorageCertificateContainerName -Context $Context#-Permission Blob
+        New-AzStorageContainer -Name $StorageGuestConfigurationContainerName -Context $Context #-Permission Blob
     }
 
 
