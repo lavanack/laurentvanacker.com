c14c4729600b98b37474a19a4c637edd5bd08de7
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-09-07 11:02:41 +0200

    Bug Fix when no GPOs Folder in SCT File

--- Windows Powershell/Windows/Security/Convert-FromSecurityComplianceToolkit/Convert-FromSecurityComplianceToolkit.ps1
+++ Windows Powershell/Windows/Security/Convert-FromSecurityComplianceToolkit/Convert-FromSecurityComplianceToolkit.ps1
@@ -33,13 +33,15 @@ param
 )
 
 #region Function Definition
+<#
+#>
 
 Function Convert-FromSecurityComplianceToolkit {
     [CmdletBinding()]
     param
     (
         [Parameter(Mandatory = $false)]
-        [string] $OutPut = Join-Path -Path $PSScriptRoot -ChildPath $(Get-Date -Format "yyyyMMddHHmmss")
+        [string] $OutPut = $(Join-Path -Path $PSScriptRoot -ChildPath $(Get-Date -Format "yyyyMMddHHmmss"))
     )
 
     #region Downloading the Microsoft Security Compliance Toolkit webpage content and extracting all download links from it
@@ -60,9 +62,9 @@ Function Convert-FromSecurityComplianceToolkit {
     
     #region Variable Definition
     #Creating subdirectories under the output root directory
-    $DSCRootDirectory = Join-Path -Path $OutPut -ChildPath "ConvertedBaselines"
-    $ExtractedBaselineDirectory = Join-Path -Path $OutPut -ChildPath "ExtractedBaselines"
-    $BaselineDirectory = Join-Path -Path $OutPut -ChildPath "Baselines"
+    $DSCRootDirectory = Join-Path -Path $OutPut -ChildPath "DSCConfigurations"
+    $ExtractedBaselineDirectory = Join-Path -Path $OutPut -ChildPath "SCTFiles"
+    $BaselineDirectory = Join-Path -Path $OutPut -ChildPath "SCTFiles"
     #endregion
 
     #Creating required directories
@@ -91,114 +93,122 @@ Function Convert-FromSecurityComplianceToolkit {
         #Getting the GPOs folder path
         $GPODir = (Get-ChildItem -Path $ConfigurationScriptPath -Filter "GPOs" -Recurse -Directory).FullName
         Write-Verbose -Message "`$GPODir: $GPODir"
-        #Directory where the converted DSC configuration scripts will be stored
-        $DSCOSDirectory = Join-Path -Path $DSCRootDirectory -ChildPath $Baseline
-        Write-Verbose -Message "`$DSCOSDirectory: $DSCOSDirectory"
-        
-        $GPOIndex = 0
-        $GPODirs = (Get-ChildItem -Path $GPODir -Directory)
-        foreach ($CurrentGPODir in $GPODirs) {
-            $GPOIndex++
-            $PercentComplete = ($GPOIndex / $GPODirs.Count * 100)
-            Write-Verbose -Message "`$PercentComplete: $PercentComplete"
-            Write-Progress -Id 2 -Activity "[$GPOIndex/$($GPODirs.Count)] Processing '$GPOName' GPO ..." -Status $("{0:N0} %" -f $PercentComplete) -PercentComplete $PercentComplete
 
-            Write-Verbose -Message "`$CurrentGPODir: $CurrentGPODir"
-            #Extracting GPO name from the bkupInfo.xml file
-            if ((Get-Content -Path "$($CurrentGPODir)\bkupInfo.xml" -Raw) -match "<GPODisplayName><!\[CDATA\[(.+)\]\]></GPODisplayName>") {
-                $GPOName = $Matches[1] -replace '\W', '_'
-                Write-Verbose -Message "`$GPOName: $GPOName"
-            }
-            else {
-                Write-Warning -Message "Cannot find GPO name in '$($CurrentGPODir)\bkupInfo.xml'."
-            }
-            Write-Host -Object "- Processing '$GPOName' GPO ..."
-            <#
+        if ($GPODir) {
+
+            #Directory where the converted DSC configuration scripts will be stored
+            $DSCOSDirectory = Join-Path -Path $DSCRootDirectory -ChildPath $Baseline
+            Write-Verbose -Message "`$DSCOSDirectory: $DSCOSDirectory"
+        
+            $GPOIndex = 0
+            $GPODirs = (Get-ChildItem -Path $GPODir -Directory)
+            foreach ($CurrentGPODir in $GPODirs) {
+                $GPOIndex++
+                $PercentComplete = ($GPOIndex / $GPODirs.Count * 100)
+                Write-Verbose -Message "`$PercentComplete: $PercentComplete"
+                Write-Progress -Id 2 -Activity "[$GPOIndex/$($GPODirs.Count)] Processing '$GPOName' GPO ..." -Status $("{0:N0} %" -f $PercentComplete) -PercentComplete $PercentComplete
+
+                Write-Verbose -Message "`$CurrentGPODir: $CurrentGPODir"
+                #Extracting GPO name from the bkupInfo.xml file
+                if ((Get-Content -Path "$($CurrentGPODir)\bkupInfo.xml" -Raw) -match "<GPODisplayName><!\[CDATA\[(.+)\]\]></GPODisplayName>") {
+                    $GPOName = $Matches[1] -replace '\W', '_'
+                    Write-Verbose -Message "`$GPOName: $GPOName"
+                }
+                else {
+                    Write-Warning -Message "Cannot find GPO name in '$($CurrentGPODir)\bkupInfo.xml'. Skipping this GPO Directory."
+                    continue
+                }
+                Write-Host -Object "- Processing '$GPOName' GPO ..."
+                <#
             $GPOName = ([regex]"<GPODisplayName><!\[CDATA\[(.+)\]\]></GPODisplayName>").Matches((Get-Content -Path "$($CurrentGPODir)\bkupInfo.xml" -Raw)).captures.groups[1].value
             Write-Verbose -Message "`$GPOName: $GPOName"
             #>
-            # !BEWARE! creating of some localhost.mof can (probably will) end with an error https://github.com/microsoft/BaselineManagement?tab=readme-ov-file#known-gaps-in-capability
-            # problematic ps1 parts have to be commented otherwise you will not be able to create DSC from it! This script will try to do it for you
-            # you will have to manually check the created configuration script and uncomment the commented parts if you want to use them
-            # you can also try to fix the generated configuration script (if any) and re-run the script
-            $DSCGPODirectory = Join-Path -Path $DSCOSDirectory -ChildPath $GPOName
-            Write-Verbose -Message "`$DSCGPODirectory: $DSCGPODirectory"
+                # !BEWARE! creating of some localhost.mof can (probably will) end with an error https://github.com/microsoft/BaselineManagement?tab=readme-ov-file#known-gaps-in-capability
+                # problematic ps1 parts have to be commented otherwise you will not be able to create DSC from it! This script will try to do it for you
+                # you will have to manually check the created configuration script and uncomment the commented parts if you want to use them
+                # you can also try to fix the generated configuration script (if any) and re-run the script
+                $DSCGPODirectory = Join-Path -Path $DSCOSDirectory -ChildPath $GPOName
+                Write-Verbose -Message "`$DSCGPODirectory: $DSCGPODirectory"
 
-            try {
-                #Conversion of the GPO to DSC configuration script
-                $ConvertedGpo = ConvertFrom-GPO -Path $CurrentGPODir -OutputConfigurationScript -OutputPath $DSCGPODirectory -ConfigName $GPOName -ShowPesterOutput -ErrorAction Stop
-                Write-Host -Object " - Successfully converted '$GPOName' GPO to DSC."
-                Write-Verbose -Message "`$ConvertedGpo: $($ConvertedGpo | Out-String)"
-            }
-            catch {
-                #Handling errors during conversion
-                if ($Error[1].ErrorDetails.Message -match "Invalid MOF definition") {
-                    $ConfigurationScript = Join-Path -Path $DSCGPODirectory -ChildPath "$GPOName.ps1"
-                    Write-Warning -Message "- In '$ConfigurationScript' comment setting that contains property mentioned in this error:`r`n'$($Error[1].ErrorDetails.Message)'.`r`nOtherwise you will not be able to generate guest configuration from it!"
-                    #Trying to extract the property name from the error message
-                    if ($Error[1].ErrorDetails.Message -match "property\s+'(\w*)'\s") {
-                        $Property = $Matches[1]
-                        Write-Verbose -Message "`$Property: $Property"
-                        # Define the regex pattern to match everything until the next }, including new lines for the faulty property
-                        $Pattern = "([^\r\n]+$Property[^\}]+\})"
+                try {
+                    #Conversion of the GPO to DSC configuration script
+                    $ConvertedGpo = ConvertFrom-GPO -Path $CurrentGPODir -OutputConfigurationScript -OutputPath $DSCGPODirectory -ConfigName $GPOName -ShowPesterOutput -ErrorAction Stop
+                    Write-Host -Object " - Successfully converted '$GPOName' GPO to DSC."
+                    Write-Verbose -Message "`$ConvertedGpo: $($ConvertedGpo | Out-String)"
+                }
+                catch {
+                    #Handling errors during conversion
+                    if ($Error[1].ErrorDetails.Message -match "Invalid MOF definition") {
+                        $ConfigurationScript = Join-Path -Path $DSCGPODirectory -ChildPath "$GPOName.ps1"
+                        Write-Warning -Message "- In '$ConfigurationScript' comment setting that contains property mentioned in this error:`r`n'$($Error[1].ErrorDetails.Message)'.`r`nOtherwise you will not be able to generate guest configuration from it!"
+                        #Trying to extract the property name from the error message
+                        if ($Error[1].ErrorDetails.Message -match "property\s+'(\w*)'\s") {
+                            $Property = $Matches[1]
+                            Write-Verbose -Message "`$Property: $Property"
+                            # Define the regex pattern to match everything until the next }, including new lines for the faulty property
+                            $Pattern = "([^\r\n]+$Property[^\}]+\})"
                         
-                        # Use regex to find all matches
-                        $FileContent = Get-Content -Path $ConfigurationScript -Raw
-                        $MyMatches = [regex]::Matches($FileContent, $Pattern)
-                        if ($MyMatches) {
-                            #Fixing the configuration script by commenting the faulty setting
-                            [regex]::Replace($FileContent, $Pattern, "<# Fixed by '$($MyInvocation.MyCommand)'`r`n`$1`r`n#>") | Set-Content -Path $ConfigurationScript
-                            Write-Verbose -Message "Commenting setting that contains property '$Property' in '$ConfigurationScript'."
-                            #Removing the faulty localhost.mof.error file if exists
-                            $null = Get-ChildItem -Path $DSCGPODirectory -Filter localhost.mof.error |  Remove-Item -ErrorAction Ignore -Force
-                            #Recalling the Configuration Script after fixing it (for generating a valid localhost.mof file)
-                            & $ConfigurationScript | Out-Null
-                            #Testing if the localhost.mof file has been successfully created
-                            if (Test-Path -Path $(Join-Path -Path $DSCGPODirectory -ChildPath localhost.mof) -PathType Leaf) {
-                                Write-Host -Message " - Repair successful!" -ForegroundColor Green
+                            # Use regex to find all matches
+                            $FileContent = Get-Content -Path $ConfigurationScript -Raw
+                            $MyMatches = [regex]::Matches($FileContent, $Pattern)
+                            if ($MyMatches) {
+                                #Fixing the configuration script by commenting the faulty setting
+                                [regex]::Replace($FileContent, $Pattern, "<# Fixed by '$($MyInvocation.MyCommand)'`r`n`$1`r`n#>") | Set-Content -Path $ConfigurationScript
+                                Write-Verbose -Message "Commenting setting that contains property '$Property' in '$ConfigurationScript'."
+                                #Removing the faulty localhost.mof.error file if exists
+                                $null = Get-ChildItem -Path $DSCGPODirectory -Filter localhost.mof.error |  Remove-Item -ErrorAction Ignore -Force
+                                #Recalling the Configuration Script after fixing it (for generating a valid localhost.mof file)
+                                & $ConfigurationScript | Out-Null
+                                #Testing if the localhost.mof file has been successfully created
+                                if (Test-Path -Path $(Join-Path -Path $DSCGPODirectory -ChildPath localhost.mof) -PathType Leaf) {
+                                    Write-Host -Message " - Repair successful!" -ForegroundColor Green
+                                }
+                                else {
+                                    Write-Warning -Message " - Repair failed!"
+                                }
                             }
-                            else {
-                                Write-Warning -Message " - Repair failed!"
-                            }
+                        }
+                        else {
+                            Write-Warning "Cannot find property name in error message: '$($Error[1].ErrorDetails.Message)'."
                         }
                     }
                     else {
-                        Write-Warning "Cannot find property name in error message: '$($Error[1].ErrorDetails.Message)'."
-                    }
-                }
-                else {
-                    Write-Error $_.Exception.Message
-                    #Trying to extract the problematic file and entry from the error message
-                    if ($_.Exception.Message -match "^.*\s(?<file>\S*)\sfile.*'(?<entry>.*)'.*unknown value.*$") {
-                        $File = $Matches['file']
-                        $Entry = $Matches['entry']
-                        Write-Verbose -Message "`$File: $File"
-                        Write-Verbose -Message "`$Entry: $Entry"
-                        Get-ChildItem -Path $CurrentGPODir -Filter $File -File -Recurse | ForEach-Object {
-                            Write-Verbose -Message "`Fixing error by removing the problematic '$Entry' entry in the '$($_.FullName)' file"
-                            $Pattern = "(^{0}.*$)" -f ($Entry -replace "(\W)", "\\$1")
-                            #(Get-Content -Path $_.FullName) -replace $Pattern, '#$1' | Set-Content -Path $_.FullName
-                            #Removing the problematic entry by replacing it with an empty string
-                            (Get-Content -Path $_.FullName) -replace $Pattern | Set-Content -Path $_.FullName
-                            Write-Verbose -Message "Commenting entry '$Entry' in '$($_.FullName)'."
-                            try {
-                                #Trying the conversion again after fixing the problematic entry
-                                $ConvertedGpo = ConvertFrom-GPO -Path $CurrentGPODir -OutputConfigurationScript -OutputPath $DSCGPODirectory -ConfigName $GPOName -ShowPesterOutput -ErrorAction Stop
-                                Write-Host -Message " - Repair successful!" -ForegroundColor Green
-                                Write-Verbose -Message "`$ConvertedGpo: $($ConvertedGpo | Out-String)"
-                            }
-                            catch {
-                                Write-Error $_.Exception.Message
-                            }
-                        }   
+                        Write-Error $_.Exception.Message
+                        #Trying to extract the problematic file and entry from the error message
+                        if ($_.Exception.Message -match "^.*\s(?<file>\S*)\sfile.*'(?<entry>.*)'.*unknown value.*$") {
+                            $File = $Matches['file']
+                            $Entry = $Matches['entry']
+                            Write-Verbose -Message "`$File: $File"
+                            Write-Verbose -Message "`$Entry: $Entry"
+                            Get-ChildItem -Path $CurrentGPODir -Filter $File -File -Recurse | ForEach-Object {
+                                Write-Verbose -Message "`Fixing error by removing the problematic '$Entry' entry in the '$($_.FullName)' file"
+                                $Pattern = "(^{0}.*$)" -f ($Entry -replace "(\W)", "\\$1")
+                                #(Get-Content -Path $_.FullName) -replace $Pattern, '#$1' | Set-Content -Path $_.FullName
+                                #Removing the problematic entry by replacing it with an empty string
+                                (Get-Content -Path $_.FullName) -replace $Pattern | Set-Content -Path $_.FullName
+                                Write-Verbose -Message "Commenting entry '$Entry' in '$($_.FullName)'."
+                                try {
+                                    #Trying the conversion again after fixing the problematic entry
+                                    $ConvertedGpo = ConvertFrom-GPO -Path $CurrentGPODir -OutputConfigurationScript -OutputPath $DSCGPODirectory -ConfigName $GPOName -ShowPesterOutput -ErrorAction Stop
+                                    Write-Host -Message " - Repair successful!" -ForegroundColor Green
+                                    Write-Verbose -Message "`$ConvertedGpo: $($ConvertedGpo | Out-String)"
+                                }
+                                catch {
+                                    Write-Error $_.Exception.Message
+                                }
+                            }   
+                        }
                     }
                 }
             }
+            Write-Progress -Id 2 -Completed -Activity 'GPO processing completed.'
         }
-        Write-Progress -Id 2 -Completed -Activity 'GPO processing completed.'
+        else {
+            Write-Warning -Message "Cannot find GPOs folder in '$ConfigurationScriptPath'. Skipping this Baseline."
+        }
     }
     Write-Progress -Id 1 -Completed -Activity 'Baseline processing completed.'
-    Get-ChildItem -Path $OutPut -Recurse -Filter localhost.mof.error
+    #Get-ChildItem -Path $OutPut -Recurse -Filter localhost.mof.error
 }
 
 #endregion
@@ -212,5 +222,5 @@ $CurrentScript = $MyInvocation.MyCommand.Path
 $CurrentDir = Split-Path -Path $CurrentScript -Parent
 Set-Location -Path $CurrentDir 
 
-Convert-FromSecurityComplianceToolkit #-Verbose
+Convert-FromSecurityComplianceToolkit -Verbose
 #endregion
