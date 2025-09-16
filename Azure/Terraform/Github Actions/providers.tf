562453dfcda0cf65058c1ab30f0ab64f333b0cc7
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-09-16 15:42:25 +0200

    Testing GitHub actions

--- Azure/Terraform/Github Actions/providers.tf
+++ Azure/Terraform/Github Actions/providers.tf
@@ -12,8 +12,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-tf-ghact-use2-7852"
-    storage_account_name = "sttfghactuse27852"
+    resource_group_name  = "rg-tf-ghact-use2-1977"
+    storage_account_name = "sttfghactuse21977"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
