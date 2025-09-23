92207ed150a3662be53c7bc014762415738e3faf
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-09-23 07:54:44 +0200

    Initial commit

---
+++ Azure/Azure Virtual Desktop/Azure Image Builder/New-CustomImageTemplateSetup.ps1
@@ -0,0 +1,291 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5 -Modules Az.Accounts, Az.Compute, Az.ImageBuilder, Az.ManagedServiceIdentity, Az.Resources
+
+#region Function definitions
+#FROM https://github.com/Azure/azvmimagebuilder/tree/main/solutions/14_Building_Images_WVD
+function New-AzureComputeGallery {
+	[CmdletBinding()]
+	Param(
+		[Parameter(Mandatory = $false)]
+		[string]$Location = "EastUS2",
+		[Parameter(Mandatory = $false)]
+		[string[]]$TargetRegions = @($Location),
+		[Parameter(Mandatory = $false)]
+		[int]$ReplicaCount = 1
+	)
+
+	#region Building an Hashtable to get the shortname of every Azure location based on a JSON file on the Github repository of the Azure Naming Tool
+	$AzLocation = Get-AzLocation | Select-Object -Property Location, DisplayName | Group-Object -Property DisplayName -AsHashTable -AsString
+	$ANTResourceLocation = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/main/src/repository/resourcelocations.json
+	$shortNameHT = $ANTResourceLocation | Select-Object -Property name, shortName, @{Name = 'Location'; Expression = { $AzLocation[$_.name].Location } } | Where-Object -FilterScript { $_.Location } | Group-Object -Property Location -AsHashTable -AsString
+	#endregion
+
+    #region Building an Hashtable to get the shortname of every Azure resource based on a JSON file on the Github repository of the Azure Naming Tool
+    $Result = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/refs/heads/main/src/repository/resourcetypes.json 
+    $ResourceTypeShortNameHT = $Result | Where-Object -FilterScript { $_.property -in @('', 'Windows') } | Select-Object -Property resource, shortName, lengthMax | Group-Object -Property resource -AsHashTable -AsString
+    #endregion
+
+	#region Set up the environment and variables
+	# get existing context
+	$AzContext = Get-AzContext
+	# Your subscription. This command gets your current subscription
+	$subscriptionID = $AzContext.Subscription.Id
+
+	#Naming convention based on https://github.com/microsoft/CloudAdoptionFramework/tree/master/ready/AzNamingTool
+	#$AzureComputeGalleryPrefix = "acg"
+	#$ResourceGroupPrefix = "rg"
+    $ResourceGroupPrefix = $ResourceTypeShortNameHT["Resources/resourcegroups"].ShortName
+    $AzureComputeGalleryPrefix = $ResourceTypeShortNameHT["Compute/galleries"].ShortName
+
+	# Location (see possible locations in the main docs)
+	Write-Verbose -Message "`$Location: $Location"
+	$LocationShortName = $shortNameHT[$Location].shortName
+	Write-Verbose -Message "`$LocationShortName: $LocationShortName"
+	if ($Location -notin $TargetRegions) {
+		$TargetRegions += $Location
+	}
+	Write-Verbose -Message "`$TargetRegions: $($TargetRegions -join ', ')"
+	[array] $TargetRegionSettings = foreach ($CurrentTargetRegion in $TargetRegions) {
+		@{"name" = $CurrentTargetRegion; "replicaCount" = $ReplicaCount; "storageAccountType" = "Premium_LRS" }
+	}
+
+	$Project = "avd"
+	$Role = "aib"
+	#Timestamp
+	$timeInt = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
+	$ResourceGroupName = "{0}-{1}-{2}-{3}-{4}" -f $ResourceGroupPrefix, $Project, $Role, $LocationShortName, $TimeInt 
+	$ResourceGroupName = $ResourceGroupName.ToLower()
+	Write-Verbose -Message "`$ResourceGroupName: $ResourceGroupName"
+
+	#region Source Image 
+	$SrcObjParamsARM = @{
+		Publisher = 'MicrosoftWindowsDesktop'
+		Offer     = 'Windows-11'    
+		Sku       = 'win11-24h2-avd'  
+		Version   = 'latest'
+	}
+	#endregion
+
+	#region Image template and definition names
+	#Image Market Place Image + customizations: VSCode
+	$imageDefinitionNameARM = "{0}-arm-vscode" -f $SrcObjParamsARM.Sku
+	$imageTemplateNameARM = "{0}-template-{1}" -f $imageDefinitionNameARM, $timeInt
+	Write-Verbose -Message "`$imageDefinitionNameARM: $imageDefinitionNameARM"
+	Write-Verbose -Message "`$imageTemplateNameARM: $imageTemplateNameARM"
+	$StagingResourceGroupNameARM = "IT_{0}_{1}_{2}" -f $ResourceGroupName, $imageTemplateNameARM.Substring(0, 13), (New-Guid).Guid
+	#endregion
+
+	# Distribution properties object name (runOutput). Gives you the properties of the managed image on completion
+	$runOutputNameARM = "cgOutputARM"
+
+	#$Version = "1.0.0"
+	$Version = Get-Date -UFormat "%Y.%m.%d"
+	$Jobs = @()
+	#endregion
+
+	#region Create resource group
+	if (Get-AzResourceGroup -Name $ResourceGroupName -Location $location -ErrorAction Ignore) {
+		Write-Verbose -Message "Removing '$ResourceGroupName' Resource Group Name ..."
+		Remove-AzResourceGroup -Name $ResourceGroupName -Force
+	}
+	Write-Verbose -Message "Creating '$ResourceGroupName' Resource Group Name ..."
+	$ResourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $location -Tag @{"SecurityControl" = "Ignore" } -Force
+
+	if (Get-AzResourceGroup -Name $StagingResourceGroupNameARM -Location $location -ErrorAction Ignore) {
+		Write-Verbose -Message "Removing '$StagingResourceGroupNameARM' Resource Group Name ..."
+		Remove-AzResource -Name $StagingResourceGroupNameARM -Force
+	}
+	Write-Verbose -Message "Creating '$StagingResourceGroupNameARM' Resource Group Name ..."
+	$StagingResourceGroupARM = New-AzResourceGroup -Name $StagingResourceGroupNameARM -Tag @{"SecurityControl" = "Ignore" } -Location $location -Force
+	#endregion
+    
+	#region Permissions, user identity, and role
+	#region setup role def names, these need to be unique
+	$imageRoleDefName = "Azure Image Builder Image Def - $timeInt"
+	$identityName = "aibIdentity-$timeInt"
+	Write-Verbose -Message "`$imageRoleDefName: $imageRoleDefName"
+	Write-Verbose -Message "`$identityName: $identityName"
+	#endregion
+
+	#region Create the identity
+	Write-Verbose -Message "Creating User Assigned Identity '$identityName' ..."
+	$AssignedIdentity = New-AzUserAssignedIdentity -ResourceGroupName $ResourceGroupName -Name $identityName -Location $location
+	#endregion
+
+	#region RBAC Assignment(s)
+	#region aibRoleImageCreation.json creation and RBAC Assignment
+	#$aibRoleImageCreationUrl="https://raw.githubusercontent.com/PeterR-msft/M365AVDWS/master/Azure%20Image%20Builder/aibRoleImageCreation.json"
+	#$aibRoleImageCreationUrl="https://raw.githubusercontent.com/azure/azvmimagebuilder/main/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+	#$aibRoleImageCreationUrl="https://raw.githubusercontent.com/lavanack/laurentvanacker.com/master/Azure/Azure%20Virtual%20Desktop/Azure%20Image%20Builder/aibRoleImageCreation.json"
+	$aibRoleImageCreationUrl = "https://raw.githubusercontent.com/lavanack/laurentvanacker.com/master/Azure/Azure%20Virtual%20Desktop/Azure%20Image%20Builder/aibRoleImageCreation.json"
+	#$aibRoleImageCreationPath = "aibRoleImageCreation.json"
+	$aibRoleImageCreationPath = Join-Path -Path $env:TEMP -ChildPath $(Split-Path $aibRoleImageCreationUrl -Leaf)
+	#Generate a unique file name 
+	$aibRoleImageCreationPath = $aibRoleImageCreationPath -replace ".json$", "_$timeInt.json"
+	Write-Verbose -Message "`$aibRoleImageCreationPath: $aibRoleImageCreationPath"
+
+	# Download the config
+	Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+	((Get-Content -Path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>', $subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+	((Get-Content -Path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $ResourceGroupName) | Set-Content -Path $aibRoleImageCreationPath
+	((Get-Content -Path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+
+	#region Create a role definition
+	Write-Verbose -Message "Creating '$imageRoleDefName' Role Definition ..."
+	$RoleDefinition = New-AzRoleDefinition -InputFile $aibRoleImageCreationPath
+	#endregion
+
+	# Grant the role definition to the VM Image Builder service principal
+	$Scope = $ResourceGroup.ResourceId
+	<#
+    if (-not(Get-AzRoleAssignment -ObjectId $AssignedIdentity.PrincipalId -RoleDefinitionName $RoleDefinition.Name -Scope $Scope)) {
+        Write-Verbose -Message "Assigning the '$($RoleDefinition.Name)' RBAC role to the '$($AssignedIdentity.PrincipalId)' System Assigned Managed Identity"
+        $RoleAssignment = New-AzRoleAssignment -ObjectId $AssignedIdentity.PrincipalId -RoleDefinitionName $RoleDefinition.Name -Scope $Scope
+        Write-Verbose -Message "`$RoleAssignment:`r`n$($RoleAssignment | Out-String)"
+    } else {
+        Write-Verbose -Message "The '$($RoleDefinition.Name)' RBAC role is already assigned to the '$($AssignedIdentity.PrincipalId)' System Assigned Managed Identity"
+    } 
+    #> 
+	$Parameters = @{
+		ObjectId           = $AssignedIdentity.PrincipalId
+		RoleDefinitionName = $RoleDefinition.Name
+		Scope              = $Scope
+	}
+
+	While (-not(Get-AzRoleAssignment @Parameters)) {
+		Write-Verbose -Message "Assigning the '$($Parameters.RoleDefinitionName)' RBAC role to the '$($Parameters.ObjectId)' System Assigned Managed Identity on the '$($Parameters.Scope)' scope"
+		try {
+			$RoleAssignment = New-AzRoleAssignment @Parameters -ErrorAction Stop
+		} 
+		catch {
+			$RoleAssignment = $null
+		}
+		Write-Verbose -Message "`$RoleAssignment:`r`n$($RoleAssignment | Out-String)"
+		if ($null -eq $RoleAssignment) {
+			Write-Verbose -Message "Sleeping 30 seconds"
+			Start-Sleep -Seconds 30
+		}
+	}
+	#endregion
+
+	#region RBAC Owner Role on both Staging Resource Groups
+	foreach ($CurrentStagingResourceGroup in $StagingResourceGroupARM) {
+		$RoleDefinition = Get-AzRoleDefinition -Name "Owner"
+		$Parameters = @{
+			ObjectId           = $AssignedIdentity.PrincipalId
+			RoleDefinitionName = $RoleDefinition.Name
+			Scope              = $CurrentStagingResourceGroup.ResourceId
+		}
+		while (-not(Get-AzRoleAssignment @Parameters)) {
+			Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Assigning the '$($Parameters.RoleDefinitionName)' RBAC role to the '$($Parameters.ObjectId)' Identity on the '$($Parameters.Scope)' scope"
+			try {
+				$RoleAssignment = New-AzRoleAssignment @Parameters -ErrorAction Stop
+			} 
+			catch {
+				$RoleAssignment = $null
+			}
+			Write-Verbose -Message "`$RoleAssignment:`r`n$($RoleAssignment | Out-String)"
+			Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Sleeping 30 seconds"
+			Start-Sleep -Seconds 30
+		}
+	}
+	#endregion
+	#endregion
+	#endregion
+
+	#region Create an Azure Compute Gallery
+	$GalleryName = "{0}_{1}_{2}_{3}" -f $AzureComputeGalleryPrefix, $Project, $LocationShortName, $timeInt
+	Write-Verbose -Message "`$GalleryName: $GalleryName"
+
+	# Create the gallery
+	Write-Verbose -Message "Creating Azure Compute Gallery '$GalleryName' ..."
+	$Gallery = New-AzGallery -GalleryName $GalleryName -ResourceGroupName $ResourceGroupName -Location $location
+	#endregion
+
+	#region Template #1 via a customized JSON file
+	#Based on https://github.com/Azure/azvmimagebuilder/tree/main/solutions/14_Building_Images_WVD
+
+	#region Create the gallery definition
+	$GalleryParams = @{
+		GalleryName       = $GalleryName
+		ResourceGroupName = $ResourceGroupName
+		Location          = $location
+		Name              = $imageDefinitionNameARM
+		OsState           = 'generalized'
+		OsType            = 'Windows'
+		Publisher         = "{0}-arm" -f $SrcObjParamsARM.Publisher
+		Offer             = "{0}-arm" -f $SrcObjParamsARM.Offer
+		Sku               = "{0}-arm" -f $SrcObjParamsARM.Sku
+		HyperVGeneration  = 'V2'
+	}
+	Write-Verbose -Message "Creating Azure Compute Gallery Image Definition '$imageDefinitionNameARM' (From ARM)..."
+	$GalleryImageDefinitionARM = New-AzGalleryImageDefinition @GalleryParams
+	#endregion
+
+	#endregion
+
+	Write-Verbose -Message "Removing '$aibRoleImageCreationPath' ..."
+	Remove-Item -Path $aibRoleImageCreationPath -Force
+	#endregion
+
+	return $Gallery
+	#endregion
+}
+#endregion
+
+#region Main code
+Clear-Host
+$Error.Clear()
+$StartTime = Get-Date
+
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir
+
+#region Login to your Azure subscription.
+While (-not(Get-AzAccessToken -ErrorAction Ignore)) {
+	Connect-AzAccount
+}
+#endregion
+
+
+#region To use Azure Image Builder, you have to register for the providers and to ensure that RegistrationState will be set to Registered.
+$RequiredResourceProviders = 'Microsoft.VirtualMachineImages', 'Microsoft.Storage', 'Microsoft.Compute', 'Microsoft.KeyVault', 'Microsoft.ManagedIdentity', 'Microsoft.Network', 'Microsoft.ContainerInstance'
+$Jobs = foreach ($CurrentRequiredResourceProvider in $RequiredResourceProviders) {
+	Register-AzResourceProvider -ProviderNamespace $CurrentRequiredResourceProvider -AsJob
+}
+#Important: Wait until RegistrationState is set to Registered. 
+While (Get-AzResourceProvider -ProviderNamespace $RequiredResourceProviders | Where-Object -FilterScript { $_.RegistrationState -ne 'Registered' }) {
+	Write-Verbose -Message "Sleeping 10 seconds ..."
+	Start-Sleep -Seconds 10
+}
+$Jobs | Remove-Job -Force
+#endregion
+
+$AzureComputeGallery = New-AzureComputeGallery -Location EastUS2 -TargetRegions EastUS2, CentralUS -Verbose
+$AzureComputeGallery
+
+$EndTime = Get-Date
+$TimeSpan = New-TimeSpan -Start $StartTime -End $EndTime
+Write-Host -Object "Total Processing Time: $($TimeSpan.ToString())"
+
+#Remove-AzResourceGroup -Name $AzureComputeGallery.ResourceGroupName -Force -AsJob
+#endregion
