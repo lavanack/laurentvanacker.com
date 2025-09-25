8473dfe6ce1a1dcaca8d731ffa4285a9c063f7bb
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-09-25 16:26:28 +0200

    Initial commit

---
+++ Azure/Misc/Get-AzSubscriptionBillingData.ps1
@@ -0,0 +1,87 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+
+#region Function definitions 
+Function Get-AzSubscriptionBillingData {
+    [CmdletBinding(PositionalBinding = $false)]
+    param(
+    )
+
+    Write-Verbose -Message "Entering function '$($MyInvocation.MyCommand)'"
+
+    #region Azure Context
+    # Log in first with Connect-AzAccount if not using Cloud Shell
+
+    $azContext = Get-AzContext
+    $SubscriptionID = $azContext.Subscription.Id
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+    $authHeader = @{
+        'Content-Type'  = 'application/json'
+        'Authorization' = 'Bearer ' + $token.AccessToken
+    }
+    #endregion
+
+    $Body = [ordered]@{ 
+        "subscriptionId" = (Get-AzContext).Subscription.Id
+    }
+
+    $BillingURI = "https://s2.billing.ext.azure.com/api/Billing/Subscription/Subscription?api-version=2019-01-14"
+    try {
+        # Invoke the REST API
+        $Response = Invoke-RestMethod -Method POST -Headers $authHeader -Body $($Body | ConvertTo-Json -Depth 100) -ContentType "application/json" -Uri $BillingURI -ErrorVariable ResponseError
+        $Response = Invoke-RestMethod -Method POST -Headers $authHeader -ContentType "application/json" -Uri $BillingURI -Body $Body -ErrorVariable ResponseError
+    }
+    catch [System.Net.WebException] {   
+        # Dig into the exception to get the Response details.
+        # Note that value__ is not a typo.
+        Write-Warning -Message "StatusCode: $($_.Exception.Response.StatusCode.value__ )"
+        Write-Warning -Message "StatusDescription: $($_.Exception.Response.StatusDescription)"
+        $respStream = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($respStream)
+        $Response = $reader.ReadToEnd() | ConvertFrom-Json
+        if (-not([string]::IsNullOrEmpty($Response.message))) {
+            Write-Warning -Message $Response.message
+        }
+    }
+    finally {
+    }
+    Write-Verbose -Message "Leaving function '$($MyInvocation.MyCommand)'"
+    return $Response
+}
+#endregion
+
+#region Main code
+Clear-Host
+$Error.Clear()
+$StartTime = Get-Date
+
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir
+
+#region Login to your Azure subscription.
+While (-not(Get-AzAccessToken -ErrorAction Ignore)) {
+	Connect-AzAccount
+}
+#endregion
+
+Get-AzSubscriptionBillingData -Verbose
+#endregion
