b03ed17c0f6d352cf7f8f520c9870723d6533b3e
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-10-20 17:33:25 +0200

    Adding scope for Install-module cmdlet

--- Azure/Desired State Configuration/Azure Machine Configuration/Windows/General/2 - Prerequisites.ps1
+++ Azure/Desired State Configuration/Azure Machine Configuration/Windows/General/2 - Prerequisites.ps1
@@ -17,18 +17,18 @@ Stop-Process -Name Explorer -Force
 #Installing the NuGet Provider
 $PreviousProgressPreferenceValue = $ProgressPreference
 $ProgressPreference = "SilentlyContinue"
-Get-PackageProvider -Name Nuget -ForceBootstrap -Force
-#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Get-PackageProvider -Name Nuget -ForceBootstrap -Scope AllUsers -Force
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope AllUsers -Force
 #For Azure
-Install-Module -Name Az.Accounts, Az.Compute, Az.PolicyInsights, Az.Resources, Az.Storage -Force
+Install-Module -Name Az.Accounts, Az.Compute, Az.PolicyInsights, Az.Resources, Az.Storage -Scope AllUsers -Force
 #For DSC
-Install-Module -Name PSDesiredStateConfiguration, PSDSCResources -Force
+Install-Module -Name PSDesiredStateConfiguration, PSDSCResources -Scope AllUsers -Force
 #For Machine/Guest Configuration
-Install-Module -Name GuestConfiguration -Force
+Install-Module -Name GuestConfiguration -Scope AllUsers -Force
 #https://github.com/microsoft/BaselineManagement?tab=readme-ov-file#install-the-module
-Install-Module -Name BaselineManagement -RequiredVersion 4.1.1 -Force
+Install-Module -Name BaselineManagement -RequiredVersion 4.1.1 -Scope AllUsers -Force
 #For Certificates
-Install-Module -Name CertificateDSC -Force
+Install-Module -Name CertificateDSC -Scope AllUsers -Force
 
 $ProgressPreference = $PreviousProgressPreferenceValue
 
