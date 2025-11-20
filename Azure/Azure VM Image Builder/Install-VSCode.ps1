f34370cc01cefadcf5522f272ba509698f1f6df7
Laurent VAN ACKER <laurent.vanacker@free.fr>
2025-11-20 12:05:21 +0100

    Switch back to UTF8 BOM

--- Azure/Azure VM Image Builder/Install-VSCode.ps1
+++ Azure/Azure VM Image Builder/Install-VSCode.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 This Sample Code is provided for the purpose of illustration only
 and is not intended to be used in a production environment.  THIS
 SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
