7489caab1603cb67c32ebc3c3ba803ec76d2d46a
Laurent VAN ACKER <laurent.vanacker@free.fr>
2026-01-09 10:55:07 +0100

    Update

--- Azure/Microsoft Entra ID/Role Assignement/Export-AzroleAssignment.ps1
+++ Azure/Microsoft Entra ID/Role Assignement/Export-AzroleAssignment.ps1
@@ -16,6 +16,46 @@ attorneys' fees, that arise or result from the use or distribution
 of the Sample Code.
 #>
 
+#region Function Definitions
+function Export-AzRoleAssignment {
+    [CmdletBinding(PositionalBinding = $false)]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Models.ApiV10.MicrosoftGraphGroup[]]$AzADGroup,
+        #To Open Excel (or the default action for CSV files)  after generating the CSV File
+        [switch]$Open
+    )
+    begin {
+        $TimeStamp = "{0:yyyyMMddHHmmss}" -f $(Get-Date)
+        $CurrentSubScription = (Get-Azcontext).SubScription
+        Write-Host -Object "Original Subscription '$($CurrentSubScription.Name) ..." -ForegroundColor ([System.ConsoleColor]::Green)
+    }
+    process {
+        foreach ($CurrentAzADGroup in $AzADGroup) {
+            Write-Host -Object "Processing '$($CurrentAzADGroup.DisplayName)' ..."
+            #Going through all subscriptions
+            foreach ($Subscription in Get-AzSubscription) {
+                #Creating a timestamped CSV File per Subscription for exporting data
+                $CSVFile = Join-Path -Path $CurrentDir -ChildPath $("{0} - Role Assignment Export - {1} - {2}.csv" -f $Timestamp, $Subscription.Name, $CurrentAzADGroup.DisplayName)
+                Write-Host -Object "Switching to '$($Subscription.Name) ..."
+                $null = Select-AzSubscription -Subscription $Subscription
+                $RoleAssignment = Get-AzRoleAssignment -Scope "/subscriptions/$($Subscription.Id)" -ObjectId $CurrentAzADGroup.Id
+                $RoleAssignment | Export-Csv -Path $CSVFile -NoTypeInformation
+                Write-Host -Object "Exporting Data to '$CSVFile' ..."
+                if ($Open) {
+                    & $CSVFile
+                }
+            }
+        }
+    }
+    end {
+        Write-Host -Object "Switching Back to the Original Subscription '$($CurrentSubScription.Name) ..." -ForegroundColor ([System.ConsoleColor]::Green)
+        #Switching back to the original Subscription
+        $CurrentSubScription | Select-AzSubscription 
+    }
+
+}
+#endregion
 
 #region Main Code
 Clear-Host
@@ -25,24 +65,12 @@ $CurrentScript = $MyInvocation.MyCommand.Path
 #Getting the current directory (where this script file resides)
 $CurrentDir = Split-Path -Path $CurrentScript -Parent
 Set-Location -Path $CurrentDir 
+#Get-AzSubscription | Out-GridView -OutputMode Single | Select-AzSubscription
 
-$TimeStamp = "{0:yyyyMMddHHmmss}" -f $(Get-Date)
-$CurrentSubScription = (Get-Azcontext).SubScription
+<#
 $DisplayName = "AVD Users"
 $AzADGroup = Get-AzADGroup -DisplayName $DisplayName
-
-#Going through all subscriptions
-foreach ($Subscription in Get-AzSubscription) {
-    #Creating a timestamped CSV File per Subscription for exporting data
-    $CSVFile = Join-Path -Path $CurrentDir -ChildPath $("{0} - Role Assignment Export - {1} - {2}.csv" -f $Timestamp, $Subscription.Name, $AzADGroup.DisplayName)
-    Write-Host -Object "Switching to '$($Subscription.Name) ..."
-    $null = Select-AzSubscription -Subscription $Subscription
-    $RoleAssignment = Get-AzRoleAssignment -Scope "/subscriptions/$($Subscription.Id)" -ObjectId $AzADGroup.Id
-    $RoleAssignment | Export-Csv -Path $CSVFile -NoTypeInformation
-    Write-Host -Object "Exporting Data to '$CSVFile' ..."
-    & $CSVFile
-}
-
-#Switching back to the original Subscription
-$CurrentSubScription | Select-AzSubscription 
+#>
+$AzADGroup = Get-AzADGroup -SearchString "AVD"
+$AzADGroup | Export-AzRoleAssignment -Verbose #-Open
 #endregion
