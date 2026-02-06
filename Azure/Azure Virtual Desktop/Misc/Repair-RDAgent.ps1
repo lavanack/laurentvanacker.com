06616e9d8a9eb897dac25ffe87dfa4eaca5191ce
Laurent VAN ACKER <laurent.vanacker@free.fr>
2026-02-06 11:37:41 +0100

    Update

--- Azure/Azure Virtual Desktop/Misc/Repair-RDAgent.ps1
+++ Azure/Azure Virtual Desktop/Misc/Repair-RDAgent.ps1
@@ -77,7 +77,7 @@ function Repair-RDAgent {
     }
     #>
 
-    $null = Get-ChildItem -Path $(Join-Path -Path $env:ProgramFiles -ChildPath "Microsoft RDInfra") -Filter *.msi | Remove-Item -Force
+    #$null = Get-ChildItem -Path $(Join-Path -Path $env:ProgramFiles -ChildPath "Microsoft RDInfra") -Filter *.msi | Remove-Item -Force
 
     $WVDAgentInstaller = Join-Path -Path $env:TEMP -ChildPath "WVD-Agent.msi"
     $WVDBootLoaderInstaller = Join-Path -Path $env:TEMP -ChildPath "WVD-BootLoader.msi"
