69932961aea5aa57abc48c2f7c81a3a156999ff9
Laurent VAN ACKER <laurent.vanacker@free.fr>
2026-03-10 15:30:41 +0100

    Update

--- Azure/Terraform/Github Actions/main.tf
+++ Azure/Terraform/Github Actions/main.tf
@@ -1,5 +1,5 @@
 #From https://learn.microsoft.com/en-us/azure/virtual-machines/windows/quick-create-terraform
-#Adding Comments v11
+#Adding Comments v12
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
