48594faa18a6b24863acdd6fbbbf80f1281fcf50
Laurent VAN ACKER <laurent.vanacker@free.fr>
2026-06-13 15:50:54 +0200

    Removing Watermarking

--- Azure/Azure Virtual Desktop/Setup/Scenarios/1 Azure Region/1_Pooled_EntraID_CloudOnly_FSLogix.ps1
+++ Azure/Azure Virtual Desktop/Setup/Scenarios/1 Azure Region/1_Pooled_EntraID_CloudOnly_FSLogix.ps1
@@ -1,3 +1,3 @@
 ﻿#Deploy 1 Pooled HostPool with Intune, FSLogix 
 #[PooledHostPool]::new($HostPoolSessionCredentialKeyVault, $PrimaryRegionSubnet.Id, $PrimaryRegionPESubnet.Id).SetJoinMode([JoinMode]::MicrosoftEntraID).SetIdentityModel([IdentityModel]::CloudOnly).EnableScalingPlan().EnableWatermarking()
-[PooledHostPool]::new($HostPoolSessionCredentialKeyVault, $PrimaryRegionSubnet.Id, $PrimaryRegionPESubnet.Id).SetIdentityModel([IdentityModel]::CloudOnly).EnableWatermarking()
+[PooledHostPool]::new($HostPoolSessionCredentialKeyVault, $PrimaryRegionSubnet.Id, $PrimaryRegionPESubnet.Id).SetIdentityModel([IdentityModel]::CloudOnly)
