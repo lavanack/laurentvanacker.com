bd4d34ab5dd66cb7260894ca1638027b82889794
Laurent VAN ACKER <laurent.vanacker@free.fr>
2026-06-26 19:19:31 +0200

    Initial commit

---
+++ Azure/Azure Virtual Desktop/Azure Virtual Desktop Hybrid/AutomatedLab - AVD Hybrid - Session Hosts.ps1
@@ -0,0 +1,262 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+
+
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+#requires -Version 5 -Modules AutomatedLab -RunAsAdministrator 
+
+trap {
+    Write-Host "Stopping Transcript ..."
+    Stop-Transcript
+    $VerbosePreference = $PreviousVerbosePreference
+    $ErrorActionPreference = $PreviousErrorActionPreference
+    [console]::beep(3000, 750)
+    Send-ALNotification -Activity 'Lab started' -Message ('Lab deployment failed !') -Provider (Get-LabConfigurationItem -Name Notifications.SubscribedProviders)
+    break
+}
+
+Import-Module -Name AutomatedLab -Verbose
+try { while (Stop-Transcript) {} } catch {}
+Clear-Host
+
+$PreviousVerbosePreference = $VerbosePreference
+$VerbosePreference = 'SilentlyContinue'
+$PreviousErrorActionPreference = $ErrorActionPreference
+$ErrorActionPreference = 'SilentlyContinue'
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+$TranscriptFile = $CurrentScript -replace ".ps1$", "_$("{0:yyyyMMddHHmmss}" -f (Get-Date)).txt"
+Start-Transcript -Path $TranscriptFile -IncludeInvocationHeader
+
+#region Global variables definition
+$Logon = 'Administrator'
+$ClearTextPassword = 'P@ssw0rd'
+$SecurePassword = ConvertTo-SecureString -String $ClearTextPassword -AsPlainText -Force
+
+$NetworkID = '10.0.0.0/16' 
+$DCIPv4Address = '10.0.0.1'
+$AVDHybrid01IPv4Address = '10.0.0.101'
+$AVDHybrid02IPv4Address = '10.0.0.102'
+
+$LabName = 'AVDHybrid'
+#endregion
+
+#Cleaning previously existing lab
+if ($LabName -in (Get-Lab -List)) {
+    Remove-Lab -Name $LabName -Confirm:$false -ErrorAction SilentlyContinue
+}
+
+#create an empty lab template and define where the lab XML files and the VMs will be stored
+New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV
+
+#make the network definition
+Add-LabVirtualNetworkDefinition -Name $LabName -HyperVProperties @{ SwitchType = 'Internal' } -AddressSpace $NetworkID
+Add-LabVirtualNetworkDefinition -Name 'Default Switch' -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Wi-Fi' }
+
+
+#these credentials are used for connecting to the Machines. As this is a lab we use clear-text passwords
+Set-LabInstallationCredential -Username $Logon -Password $ClearTextPassword
+
+#defining default parameter values, as these ones are the same for all the Machines
+$PSDefaultParameterValues = @{
+    'Add-LabMachineDefinition:Network'         = $LabName
+    'Add-LabMachineDefinition:MinMemory'       = 4GB
+    'Add-LabMachineDefinition:MaxMemory'       = 8GB
+    'Add-LabMachineDefinition:Memory'          = 4GB
+    'Add-LabMachineDefinition:OperatingSystem' = 'Windows 11 Enterprise'
+    'Add-LabMachineDefinition:Processors'      = 2
+}
+
+$AVDHybrid01NetAdapter = @()
+$AVDHybrid01NetAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $LabName -Ipv4Address $AVDHybrid01IPv4Address -InterfaceName Corp
+#Adding an Internet Connection on the DC (Required for PowerShell Gallery)
+$AVDHybrid01NetAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch 'Default Switch' -UseDhcp -InterfaceName Internet
+
+$AVDHybrid02NetAdapter = @()
+$AVDHybrid02NetAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $LabName -Ipv4Address $AVDHybrid02IPv4Address -InterfaceName Corp
+#Adding an Internet Connection on the DC (Required for PowerShell Gallery)
+$AVDHybrid02NetAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch 'Default Switch' -UseDhcp -InterfaceName Internet
+
+
+#region server definitions
+#AVDHybrid-01 
+Add-LabMachineDefinition -Name AvdHybrid-01 -NetworkAdapter $AVDHybrid01NetAdapter
+#AVDHybrid-02
+Add-LabMachineDefinition -Name AvdHybrid-02 -NetworkAdapter $AVDHybrid02NetAdapter
+#endregion
+
+#Installing servers
+Install-Lab -Verbose
+#Checkpoint-LabVM -SnapshotName FreshInstall -All -Verbose
+
+#region Installing Required Windows Features
+$Machines = Get-LabVM
+
+Install-LabWindowsFeature -FeatureName Telnet-Client -ComputerName $Machines -IncludeManagementTools -AsJob
+#endregion
+
+Invoke-LabCommand -ActivityName 'Windows Virtual Desktop Optimization Tool (VDOT)' -ComputerName $Machines -ScriptBlock {
+    winget install --id Git.Git -e --source winget
+    $GitHubRootDir = Join-Path -Path $env:SystemDrive -ChildPath "Source Control\GitHub"
+    $GitHubRepoURI = "https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool"
+    $GitHubRepoName = Split-Path -Path $GitHubRepoURI -Leaf
+    $GitHubRepoDir = Join-Path -Path $GitHubRootDir -ChildPath $GitHubRepoName
+
+    $Directory = New-Item -Path $GitHubRootDir -ItemType Directory -Force
+    Start-Process -FilePath "$env:comspec" -ArgumentList "/c", "git clone $GitHubRepoURI ""$GitHubRepoDir""" -Wait -WorkingDirectory "$env:ProgramFiles\Git\cmd"
+    <#
+    Set-Location -Path $GitHubRepoDir
+    .\Windows_VDOT.ps1 -Optimizations All -AdvancedOptimizations All -AcceptEULA -Verbose
+    #>
+    $ScriptFile = Join-Path -Path $GitHubRepoDir -ChildPath "Windows_VDOT.ps1"
+    & $ScriptFile -Optimizations All -AdvancedOptimizations All -AcceptEULA -Verbose
+    Restart-Computer -Force
+}
+
+
+Get-Job -Name 'Installation of*' | Wait-Job | Out-Null
+
+Checkpoint-LabVM -SnapshotName 'FullInstall' -All
+
+Show-LabDeploymentSummary
+
+$VerbosePreference = $PreviousVerbosePreference
+$ErrorActionPreference = $PreviousErrorActionPreference
+#Restore-LabVMSnapshot -SnapshotName 'FullInstall' -All -Verbose
+
+Stop-Transcript
+
+#region Azure
+#region Login to your Azure subscription.
+While (-not(Get-AzAccessToken -ErrorAction Ignore)) {
+    Connect-AzAccount -UseDeviceAuthentication
+}
+#endregion
+
+Install-Module -Name Az.DesktopVirtualization, Az.ConnectedMachine -AllowClobber -Force -Verbose 
+
+#region Host Pool Management
+$ResourceGroup = Get-AzResourceGroup -Name rg-hp-pd-ei-hyb-mp-*
+if ($ResourceGroup) {
+    if ($ResourceGroup.count -gt 1) {
+        $ResourceGroup = $ResourceGroup | Out-GridView -OutputMode Single
+    }
+    $PersonalHostPool = Get-AzWvdHostPool -ResourceGroupName $ResourceGroup.ResourceGroupName
+    if ($PersonalHostPool -and $PersonalHostPool.HostPoolType -eq 'Personal') {
+        if ($PersonalHostPool.count -gt 1) {
+            $PersonalHostPool = $PersonalHostPool | Out-GridView -OutputMode Single
+        }
+        $PersonalHostPool = Get-AzWvdHostPool -ResourceGroupName $ResourceGroup.ResourceGroupName | Select-Object -First 1
+        $Location = $ResourceGroup.Location
+        $Context = Get-AzContext
+        $SubscriptionId = $Context.Subscription.Id
+        $TenantId = $Context.Tenant.Id
+
+        #region Azure Arc Onboarding
+        $ScriptBlockContent = @"
+Get-PackageProvider -Name Nuget -ForceBootstrap -Force
+Install-Module -Name Az.DesktopVirtualization, Az.ConnectedMachine -AllowClobber -Force -Verbose 
+#region Login to your Azure subscription.
+While (-not(Get-AzAccessToken -ErrorAction Ignore)) {
+Connect-AzAccount -UseDeviceAuthentication
+}
+Connect-AzConnectedMachine -ResourceGroupName $($ResourceGroup.ResourceGroupName) -Name `$env:COMPUTERNAME -Location $Location
+Write-Host -Object "Done ..." -ForegroundColor Green
+"@
+
+        $FilePath = Join-Path -Path $env:SystemDrive -ChildPath "AzureArcOnboarding.ps1"
+        Invoke-LabCommand -ActivityName 'Azure Arc Onboarding' -ComputerName $Machines -ScriptBlock {
+            $using:ScriptBlockContent | Out-File -FilePath $using:FilePath
+        } #-AsJob
+
+        Do {
+            $Input = Read-Host -Prompt "Connect via RDP to $($Machines.Name -join ', ') and run the '$FilePath' script before continuing ...`r`nPress Y to continue"
+        } While ($Input -ne 'Y')
+
+        #region Check
+        Start-Process "https://portal.azure.com/#servicemenu/Microsoft_Azure_ArcCenterUX/AzureArcCenterHub/servers"
+        Get-AzConnectedMachine -ResourceGroupName $($ResourceGroup.ResourceGroupName)
+        #endregion 
+        #endregion 
+
+        #region EntraID join
+        $settings = @{
+            # IMPORTANT: must be present even empty
+            mdmId = ""   
+        }
+        foreach($Machine in $Machines) {
+            New-AzConnectedMachineExtension -Name "aadlogin" -ResourceGroupName $ResourceGroup.ResourceGroupName -MachineName $Machine.Name -Location $Location -Publisher "Microsoft.Azure.ActiveDirectory" -ExtensionType "AADLoginForWindows" -Settings $settings
+        }
+        #endregion
+
+        #region Generate a host pool registration key
+        $ExpiresUtc = (Get-Date).ToUniversalTime().AddDays(1).ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")
+        $RegistrationInfo = New-AzWvdRegistrationInfo -ResourceGroupName $PersonalHostPool.ResourceGroupName -HostPoolName $PersonalHostPool.Name -ExpirationTime $ExpiresUtc
+        $RegistrationToken = $RegistrationInfo.Token
+        #endregion
+
+        #region Installing the Arc Extension
+        # Settings
+        $settings          = @{ isCloudDevice = $false }
+        $protectedSettings = @{ registrationToken = $RegistrationToken }
+
+        #Installing the Arc Extension
+        foreach($Machine in $Machines) {
+            Write-Host "Install the Arc Extension on '$($Machine.Name)' ..."
+            New-AzConnectedMachineExtension -Name 'Microsoft.AzureVirtualDesktop.CloudDeviceExtension' -ResourceGroupName $ResourceGroup.ResourceGroupName -MachineName $Machine.Name -Location $Location -Publisher 'Microsoft.AzureVirtualDesktop' -ExtensionType 'CloudDeviceExtension' -Setting $settings -ProtectedSetting $protectedSettings -verbose
+            Get-AzConnectedMachineExtension -ResourceGroupName $ResourceGroup.ResourceGroupName -MachineName $Machine.Name -Name ‘Microsoft.AzureVirtualDesktop.CloudDeviceExtension’
+        }
+        #endregion
+
+        #region RBAC Assignments
+        #region "Virtual Machine User Login"
+        $ConnectedMachines = Get-AzConnectedMachine -ResourceGroupName $ResourceGroup.ResourceGroupName | Where-Object -FilterScript { $_.Name -in $Machines.Name}
+        $RoleDefinition = Get-AzRoleDefinition -Name "Virtual Machine User Login"
+        $AzADGroup = Get-AzADGroup -DisplayName "AVD Users"
+
+        foreach($ConnectedMachine in $ConnectedMachines) {
+            $Parameters = @{
+                ObjectId           = $AzADGroup.Id
+                RoleDefinitionName = $RoleDefinition.Name
+                Scope              = $ConnectedMachine.Id
+                #Verbose            = $true
+            }
+            New-AzRoleAssignment @Parameters
+        }
+        #endregion
+
+        #region "Desktop Virtualization User" to Desktop Application Group
+        $ConnectedMachines = Get-AzConnectedMachine -ResourceGroupName $ResourceGroup.ResourceGroupName | Where-Object -FilterScript { $_.Name -in $Machines.Name}
+        $RoleDefinition = Get-AzRoleDefinition -Name "Desktop Virtualization User"
+        $AzADGroup = Get-AzADGroup -DisplayName "AVD Users"
+        $ApplicationGroup = Get-AzWvdApplicationGroup -ResourceGroupName $ResourceGroup.ResourceGroupName
+        $Parameters = @{
+            ObjectId           = $AzADGroup.Id
+            ResourceName       = $ApplicationGroup.Name
+            ResourceGroupName  = $ResourceGroup.ResourceGroupName
+            RoleDefinitionName = $RoleDefinition.Name
+            ResourceType       = 'Microsoft.DesktopVirtualization/applicationGroups'
+            #Verbose            = $true
+        }
+        New-AzRoleAssignment @Parameters
+        #endregion
+        #endregion
+    }
+}
+
+#endregion
---
+++ Azure/Azure Virtual Desktop/Azure Virtual Desktop Hybrid/New-AzAvdHybridEntraIDPersonalHostPoolSetup.ps1
@@ -0,0 +1,243 @@
+﻿<#
+This Sample Code is provided for the purpose of illustration only
+and is not intended to be used in a production environment.  THIS
+SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+right to use and modify the Sample Code and to reproduce and distribute
+the object code form of the Sample Code, provided that You agree:
+(i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is
+embedded; and (iii) to indemnify, hold harmless, and defend Us and
+Our suppliers from and against any claims or lawsuits, including
+attorneys' fees, that arise or result from the use or distribution
+of the Sample Code.
+#>
+
+#requires -Modules Az.Accounts, Az.Compute, Az.DesktopVirtualization, Az.KeyVault, Az.Network, Az.Resources
+#From https://learn.microsoft.com/en-us/azure/virtual-desktop/deploy-azure-virtual-desktop?pivots=host-pool-session-host-configuration&tabs=portal-standard%2Cpowershell-session-host-configuration%2Cportal#create-a-host-pool-with-a-session-host-configuration
+
+#region function definitions 
+function New-AzAvdHybridEntraIDPersonalHostPoolSetup {
+    [CmdletBinding(PositionalBinding = $false)]
+    param
+    (
+        [ValidateScript({ $_ -in (Get-AzLocation).Location })]
+        [string] $Location = "centralus",
+        [Parameter(Mandatory = $true)]
+        [ValidatePattern("/subscriptions/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/resourceGroups/.+/providers/Microsoft\.Network/virtualNetworks/.+/subnets/.+")] 
+        [string]$SubNetId = "/subscriptions/30c8d9eb-366e-4d2c-a723-95bc688f7c97/resourceGroups/rg-avd-ad-usc-002/providers/Microsoft.Network/virtualNetworks/vnet-avd-avd-usc-002/subnets/snet-avd-avd-usc-002"
+    )
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Entering function '$($MyInvocation.MyCommand)'"
+
+    #region Building an Hashtable to get the shortname of every Azure location based on a JSON file on the Github repository of the Azure Naming Tool
+    $AzLocation = Get-AzLocation | Select-Object -Property Location, DisplayName | Group-Object -Property DisplayName -AsHashTable -AsString
+    $ANTResourceLocation = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/main/src/repository/resourcelocations.json
+    $shortNameHT = $ANTResourceLocation | Select-Object -Property name, shortName, @{Name = 'Location'; Expression = { $AzLocation[$_.name].Location } } | Where-Object -FilterScript { $_.Location } | Group-Object -Property Location -AsHashTable -AsString
+    #endregion
+
+    #region Building an Hashtable to get the prefix of every Azure resource type based on a JSON file on the Github repository of the Azure Naming Tool
+    $Result = Invoke-RestMethod -Uri https://raw.githubusercontent.com/mspnp/AzureNamingTool/refs/heads/main/src/repository/resourcetypes.json 
+    $ResourceTypeShortNameHT = $Result | Where-Object -FilterScript { $_.property -notin @('Linux') } | Select-Object -Property resource, shortName, property, lengthMax | Group-Object -Property resource -AsHashTable -AsString
+    #endregion
+
+    #region Resource Naming
+    $AzureVMNameMaxLength = $ResourceTypeShortNameHT["Compute/virtualMachines"].lengthMax
+    $LocationShortName = $shortNameHT[$Location].shortName
+    #Naming convention based on https://github.com/microsoft/CloudAdoptionFramework/tree/master/ready/AzNamingTool
+    $ResourceGroupNamePrefix = $ResourceTypeShortNameHT["Resources/resourcegroups"].ShortName
+    $KeyVaultPrefix = $ResourceTypeShortNameHT["KeyVault/vaults"].ShortName
+    $DigitNumber = 3
+    Do {
+        $Instance = Get-Random -Minimum 0 -Maximum $([long]([Math]::Pow(10, $DigitNumber)))
+        $HostPoolName = "hp-pd-ei-hyb-mp-{0}-{1:D3}" -f $LocationShortName, $Instance
+        $LogAnalyticsWorkSpaceName = "log{0}" -f $($HostPoolName -replace "\W")
+        $KeyVaultName = "{0}{1}" -f $KeyVaultPrefix, $($HostPoolName -replace "\W")
+        $ResourceGroupName = "{0}-{1}" -f $ResourceGroupNamePrefix, $HostPoolName
+    } while (-not(Test-AzKeyVaultNameAvailability -Name $KeyVaultName).NameAvailable)
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] `$HostPoolName: $HostPoolName"
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] `$KeyVaultName: $KeyVaultName"
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] `$ResourceGroupName: $ResourceGroupName"
+    #endregion 
+
+    #region ResourceGroup
+    $ResourceGroup = Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction Ignore 
+    if ($null -eq $ResourceGroup) {
+        Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Creating the '$ResourceGroupName' ResourceGroup"
+        $ResourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force
+    }
+    #endregion
+
+    #region Log Analytics WorkSpace
+    $LogAnalyticsWorkSpace = New-AzOperationalInsightsWorkspace -Location $Location -Name $LogAnalyticsWorkSpaceName -Sku pergb2018 -ResourceGroupName $ResourceGroup.ResourceGroupName -Force
+    #endregion
+
+    #region HostPool Setup
+    #region Create a HostPool
+    $CurrentHostPool = [PSCustomObject] @{
+        Name                            = $HostPoolName
+        GetSessionHostConfigurationName = $ResourceGroupName -replace "^rg", "shc"
+        LoadBalancerType                = "Persistent"
+        PreferredAppGroupType           = "Desktop"
+        Location                        = $Location
+        NamePrefix                      = "nem{0}{1:D3}" -f $LocationShortName, $Instance
+        VMSize                          = "Standard_D2s_v5"
+        SubnetId                        = $SubNetId
+        ImagePublisherName              = "microsoftwindowsdesktop"
+        ImageOffer                      = "office-365"
+        ImageSku                        = "win11-24h2-avd-m365"
+        VMNumberOfInstances             = 1
+        ResourceGroupName               = $ResourceGroupName
+        WorkSpaceName                   = $ResourceGroupName -replace "^rg", "ws"
+        ScalingPlan                     = $true
+        #Installing VS Code on All AVD Session Hosts
+        CustomConfigurationScriptUrl    = "https://raw.githubusercontent.com/lavanack/laurentvanacker.com/refs/heads/master/Azure/Azure%20VM%20Image%20Builder/Install-VSCode.ps1"
+    }
+
+    $Parameters = @{
+        Name                  = $CurrentHostPool.Name
+        ResourceGroupName     = $ResourceGroupName
+        HostPoolType          = 'Personal'
+        LoadBalancerType      = $CurrentHostPool.LoadBalancerType
+        PreferredAppGroupType = $CurrentHostPool.PreferredAppGroupType
+        Location              = $CurrentHostPool.Location
+        StartVMOnConnect      = $true
+        # From https://learn.microsoft.com/en-us/windows-server/remote/remote-desktop-services/clients/rdp-files?context=%2Fazure%2Fvirtual-desktop%2Fcontext%2Fcontext#device-redirection
+        # No RDP redirection for COM ports, Local Drives and printers.
+        ExpirationTime        = (Get-Date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ')
+        CustomRdpProperty     = $CustomRdpProperty
+        IdentityType          = "SystemAssigned"
+        Tag                   = $Tag
+        ManagementType        = 'Standard'
+        ValidationEnvironment = $true
+        #Verbose               = $true
+    }
+    $CurrentAzWvdHostPool = New-AzWvdHostPool @Parameters
+    #endregion
+
+    #region RBAC Assignments to the HostPool System-Assigned Managed Identity
+    $ObjectId = $CurrentAzWvdHostPool.IdentityPrincipalId
+
+    #region 'Desktop Virtualization Virtual Machine Contributor' RBAC Assignment
+    $NsgId = (Get-AzVirtualNetworkSubnetConfig -ResourceId $SubNetId).NetworkSecurityGroup.Id
+    $vNetId = $SubNetId -replace "/subnets/.*"
+    $Scopes = (Get-AzResourceGroup -ResourceGroupName $CurrentHostPool.ResourceGroupName).ResourceId, $vNetId, $NsgId
+    #/subscriptions/30c8d9eb-366e-4d2c-a723-95bc688f7c97/resourceGroups/rg-avd-aib-usc-1750417854/providers/Microsoft.Compute/galleries/acg_avd_usc_1750417854/images/win11-24h2-avd-json-vscode/versions/2025.06.20
+    if ($CurrentHostPool.VMSourceImageId) {
+        #$ACGResourceGroupId = $(Get-AzresourceGroup  -ResourceGroupName $((Get-AzResource -ResourceId $CurrentHostPool.VMSourceImageId).ResourceGroupName)).ResourceId
+        $ACGResourceGroupId = $CurrentHostPool.VMSourceImageId -replace "/providers/.+"
+        $Scopes += $ACGResourceGroupId
+    }
+    $RoleDefinition = Get-AzRoleDefinition -Name "Desktop Virtualization Virtual Machine Contributor"
+    foreach ($Scope in $Scopes) {
+        $Parameters = @{
+            ObjectId           = $ObjectId
+            RoleDefinitionName = $RoleDefinition.Name
+            Scope              = $Scope
+        }
+        while (-not(Get-AzRoleAssignment @Parameters)) {
+            Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Assigning the '$($Parameters.RoleDefinitionName)' RBAC role to the '$($Parameters.SignInName)' Identity on the '$($Parameters.Scope)' scope"
+            $RoleAssignment = New-AzRoleAssignment @Parameters -ErrorAction Ignore
+            Write-Verbose -Message "`$RoleAssignment:`r`n$($RoleAssignment | Out-String)"
+            Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Sleeping 30 seconds"
+            Start-Sleep -Seconds 30
+        }
+    }
+    #endregion 
+    #endregion
+
+    #region Enabling Diagnostics Setting for the HostPool
+    $Log = New-AzDiagnosticSettingLogSettingsObject -Enabled $true -CategoryGroup allLogs 
+    $HostPoolDiagnosticSetting = New-AzDiagnosticSetting -Name $CurrentAzWvdHostPool.Name -ResourceId $CurrentAzWvdHostPool.Id -WorkspaceId $LogAnalyticsWorkSpace.ResourceId -Log $Log
+    #endregion
+    #endregion
+
+    #region Desktop Application Group Setup
+    #region Create a Desktop Application Group
+    $Parameters = @{
+        Name                 = "{0}-DAG" -f $CurrentHostPool.Name
+        #FriendlyName         = $CurrentHostPool.Name
+        ResourceGroupName    = $CurrentHostPool.ResourceGroupName
+        Location             = $CurrentHostPool.Location
+        HostPoolArmPath      = $CurrentAzWvdHostPool.Id
+        ApplicationGroupType = 'Desktop'
+        ShowInFeed           = $true
+        #Verbose              = $true
+    }
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Creating the Desktop Application Group for the '$($CurrentHostPool.Name)' Host Pool (in the '$($CurrentHostPool.ResourceGroupName)' Resource Group)"
+    $CurrentAzDesktopApplicationGroup = New-AzWvdApplicationGroup @Parameters
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] The Desktop Application Group for the '$($CurrentHostPool.Name)' Host Pool (in the '$($CurrentHostPool.ResourceGroupName)' Resource Group) is created"
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Updating the friendly name of the Desktop for the Desktop Application Group of the '$($CurrentHostPool.Name)' Host Pool (in the '$($CurrentHostPool.ResourceGroupName)' Resource Group) to '$($CurrentHostPool.Name)'"
+    $Parameters = @{
+        ApplicationGroupName = $CurrentAzDesktopApplicationGroup.Name
+        ResourceGroupName    = $CurrentHostPool.ResourceGroupName
+    }
+    $null = Get-AzWvdDesktop @parameters | Update-AzWvdDesktop -FriendlyName $CurrentHostPool.Name
+    #endregion
+
+    #region Enabling Diagnostics Setting for the Desktop Application Group
+    $Log = New-AzDiagnosticSettingLogSettingsObject -Enabled $true -CategoryGroup allLogs 
+    $DesktopApplicationGroupDiagnosticSetting = New-AzDiagnosticSetting -Name $CurrentAzDesktopApplicationGroup.Name -ResourceId $CurrentAzDesktopApplicationGroup.Id -WorkspaceId $LogAnalyticsWorkSpace.ResourceId -Log $Log
+    #endregion
+    #endregion
+
+    #region Workspace Setup
+    #region Create a Workspace
+    $ApplicationGroupReference = $CurrentAzDesktopApplicationGroup.Id
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] `$ApplicationGroupReference: $($ApplicationGroupReference -join ', ')"
+
+    $Parameters = @{
+        Name                      = $CurrentHostPool.WorkSpaceName
+        FriendlyName              = $FriendlyName
+        ResourceGroupName         = $CurrentHostPool.ResourceGroupName
+        ApplicationGroupReference = $ApplicationGroupReference
+        Location                  = $CurrentHostPool.Location
+        #Verbose                   = $true
+    }
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Creating the WorkSpace for the '$($CurrentHostPool.Name)' Host Pool (in the '$($CurrentHostPool.ResourceGroupName)' Resource Group)"
+    $CurrentAzWvdWorkspace = New-AzWvdWorkspace @Parameters
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] The WorkSpace for the '$($CurrentHostPool.Name)' Host Pool (in the '$($CurrentHostPool.ResourceGroupName)' Resource Group) is created"
+    #endregion
+
+    #region Enabling Diagnostics Setting for the WorkSpace
+    $Log = New-AzDiagnosticSettingLogSettingsObject -Enabled $true -CategoryGroup allLogs 
+    $WorkSpaceDiagnosticSetting = New-AzDiagnosticSetting -Name $CurrentAzWvdWorkspace.Name -ResourceId $CurrentAzWvdWorkspace.Id -WorkspaceId $LogAnalyticsWorkSpace.ResourceId -Log $Log
+    #endregion
+    #endregion
+
+    Write-Verbose -Message "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")][$($MyInvocation.MyCommand)] Leaving function '$($MyInvocation.MyCommand)'"
+
+    return $CurrentAzWvdHostPool
+}
+#endregion
+
+#region Main code
+Clear-Host
+$CurrentScript = $MyInvocation.MyCommand.Path
+#Getting the current directory (where this script file resides)
+$CurrentDir = Split-Path -Path $CurrentScript -Parent
+Set-Location -Path $CurrentDir
+ 
+#region Login to your Azure subscription.
+While (-not(Get-AzAccessToken -ErrorAction Ignore)) {
+    Connect-AzAccount
+}
+#endregion
+
+$SubscriptionId = (Get-AzContext).Subscription.Id
+$Location = "centralus"
+
+
+$Parameters = @{
+    Location             = $Location 
+    SubNetId             = "/subscriptions/{0}/resourceGroups/rg-avd-ad-usc-002/providers/Microsoft.Network/virtualNetworks/vnet-avd-avd-usc-002/subnets/snet-avd-avd-usc-002" -f $SubscriptionId
+    Verbose              = $true
+}
+$PersonalHostPool = New-AzAvdHybridEntraIDPersonalHostPoolSetup @Parameters
+#endregion
