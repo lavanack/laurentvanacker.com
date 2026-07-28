73f8171d31d93c39943b5b71c1b501f9a7fc4e3b
Laurent VAN ACKER <laurent.vanacker@free.fr>
2026-07-28 12:52:48 +0200

    Update

--- Azure/Azure Virtual Desktop/AAD-Hybrid-Lab - PowerShell/New-AAD-Hybrid-Lab.ps1
+++ Azure/Azure Virtual Desktop/AAD-Hybrid-Lab - PowerShell/New-AAD-Hybrid-Lab.ps1
@@ -933,6 +933,7 @@ $UserCredential = Get-Credential -Credential "Only password is required"
 #Fixed instance number so resource names are deterministic across runs
 #$Instance = Get-Random -Minimum 1 -Maximum 1000
 $Instance = 1
+$CustomUPNSuffix = $(Get-AzTenant).Domains[-1]
 
 #Splatted parameters passed to the lab deployment function
 $Parameters = @{
@@ -943,6 +944,7 @@ $Parameters = @{
     "Project"              = "avd"
     "Role"                 = "ad"
     "ADDomainName"         = "csa.fr"
+    #"CustomUPNSuffix"      = $CustomUPNSuffix
     "CustomUPNSuffix"      = "cloudsolutionarchitect.fr"
     "VNetAddressRange"     = '10.0.0.0/16'
     "ADSubnetAddressRange" = '10.0.1.0/24'
